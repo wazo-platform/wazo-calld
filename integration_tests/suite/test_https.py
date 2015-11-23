74474167543882eb4a31db2186b5162e84100a8d
Sebastien Duthil <sduthil@avencall.com>
2015-11-23 09:47:00 -0500

    fix semi-failing integration test
    reason: currently, xivo-ctid-ng will only stop once it is connected to
    rabbitmq. When running integration tests, xivo-ctid-ng and rabbitmq
    start in parallel, so xivo-ctid-ng must retry once or twice before
    successfully connecting to rabbitmq, which may take a little bit more
    than 2 seconds.

--- integration_tests/suite/test_https.py
+++ integration_tests/suite/test_https.py
@@ -25,7 +25,7 @@ class TestHTTPSMissingCertificate(IntegrationTest):
     asset = 'no_ssl_certificate'
 
     def test_given_inexisting_SSL_certificate_when_dird_starts_then_dird_stops(self):
-        for _ in range(5):
+        for _ in range(10):
             status = self.service_status()
             if not status['State']['Running']:
                 break
@@ -41,7 +41,7 @@ class TestHTTPSMissingPrivateKey(IntegrationTest):
     asset = 'no_ssl_private_key'
 
     def test_given_inexisting_SSL_private_key_when_dird_starts_then_dird_stops(self):
-        for _ in range(2):
+        for _ in range(10):
             status = self.service_status()
             if not status['State']['Running']:
                 break
