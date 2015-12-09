6c394e62019f6baf631b93cdc9552dce35ff2675
Sebastien Duthil <sduthil@avencall.com>
2015-12-09 10:28:43 -0500

    extract Call to its own module

--- xivo_ctid_ng/plugins/calls/resources.py
+++ xivo_ctid_ng/plugins/calls/resources.py
@@ -25,6 +25,7 @@ from contextlib import contextmanager
 from xivo_confd_client import Client as ConfdClient
 from xivo_ctid_ng.core.rest_api import AuthResource
 
+from .call import Call
 from .exceptions import AsteriskARIUnreachable
 from .exceptions import CallCreationError
 from .exceptions import InvalidUserUUID
@@ -92,27 +93,6 @@ def get_uuid_from_channel_id(ari, channel_id):
             raise XiVOConfdUnreachable(current_app.config['confd'], e)
 
     return None
-
-
-class Call(object):
-
-    def __init__(self, id_, creation_time):
-        self.id_ = id_
-        self.creation_time = creation_time
-        self.bridges = []
-        self.status = 'Down'
-        self.talking_to = []
-        self.user_uuid = None
-
-    def to_dict(self):
-        return {
-            'bridges': self.bridges,
-            'call_id': self.id_,
-            'creation_time': self.creation_time,
-            'status': self.status,
-            'talking_to': self.talking_to,
-            'user_uuid': self.user_uuid,
-        }
 
 
 def get_channel_ids_from_bridges(ari, bridges):
