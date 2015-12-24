bfc4bc82fe168478a0e18d97819ed75c409a9838
Sebastien Duthil <sduthil@avencall.com>
2015-12-24 11:41:51 -0500

    answer channels before bridging them
    reason: sip phones need to be answered before talking

--- xivo_ctid_ng/plugins/calls/stasis.py
+++ xivo_ctid_ng/plugins/calls/stasis.py
@@ -28,9 +28,13 @@ class CallsStasis(object):
         if not event.get('args'):
             return
 
+        channel = event_objects['channel']
         if event['args'][0] == 'dialed_from':
             originator_channel_id = event['args'][1]
-            this_channel_id = event_objects['channel'].id
+            originator_channel = self.ari.channels.get(channelId=originator_channel_id)
+            channel.answer()
+            originator_channel.answer()
+            this_channel_id = channel.id
             bridge = self.ari.bridges.create(type='mixing')
             bridge.addChannel(channel=originator_channel_id)
             bridge.addChannel(channel=this_channel_id)
