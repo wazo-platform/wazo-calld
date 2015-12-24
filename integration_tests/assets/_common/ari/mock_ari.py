e095b8bbc6084e1f7291ecd25adb2685f3237cfa
Sebastien Duthil <sduthil@avencall.com>
2015-12-24 11:41:51 -0500

    answer channels before bridging them
    reason: sip phones need to be answered before talking

--- integration_tests/assets/_common/ari/mock_ari.py
+++ integration_tests/assets/_common/ari/mock_ari.py
@@ -118,6 +118,11 @@ def delete_channel(channel_id):
     return '', 204
 
 
+@app.route('/ari/channels/<channel_id>/answer', methods=['POST'])
+def answer(channel_id):
+    return '', 204
+
+
 @app.route('/ari/bridges')
 def list_bridges():
     result = [bridge for bridge in _responses['bridges'].itervalues()]
