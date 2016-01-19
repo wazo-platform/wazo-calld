a381751dde933a50234fd2996c79ea5e293ef3c9
Sebastien Duthil <sduthil@avencall.com>
2016-01-19 09:34:42 -0500

    extract AuthVerifier to xivo-lib-python

--- xivo_ctid_ng/core/auth.py
+++ xivo_ctid_ng/core/auth.py
@@ -3,60 +3,9 @@
 # SPDX-License-Identifier: GPL-3.0+
 
 import logging
-import requests
 
-from flask import request
-from flask_restful import abort
-from functools import wraps
-from time import time
-
-from xivo_auth_client import Client
+from xivo import auth_helpers
 
 logger = logging.getLogger(__name__)
 
-
-def required_acl(acl):
-    def wrapper(func):
-        func.acl = acl
-        return func
-    return wrapper
-
-
-class AuthVerifier(object):
-
-    def __init__(self):
-        self._auth_config = None
-
-    def set_config(self, auth_config):
-        self._auth_config = dict(auth_config)
-        self._auth_config.pop('username', None)
-        self._auth_config.pop('password', None)
-        self._auth_config.pop('key_file', None)
-        self._auth_client = Client(**self._auth_config)
-
-    def verify_token(self, func):
-        @wraps(func)
-        def wrapper(*args, **kwargs):
-            token = request.headers.get('X-Auth-Token', '')
-
-            assert self._auth_client, 'AuthVerifier is not configured'
-
-            try:
-                required_acl = getattr(func, 'acl', '').format(**kwargs)
-                token_is_valid = self._auth_client.token.is_valid(token, required_acl)
-            except requests.RequestException as e:
-                auth_host = self._auth_config['host']
-                auth_port = self._auth_config['port']
-                message = 'Could not connect to authentication server on {host}:{port}: {error}'.format(host=auth_host, port=auth_port, error=e)
-                logger.exception(message)
-                return {
-                    'reason': [message],
-                    'timestamp': [time()],
-                    'status_code': 503,
-                }, 503
-
-            if token_is_valid:
-                return func(*args, **kwargs)
-
-            abort(401)
-        return wrapper
+required_acl = auth_helpers.required_acl
--- xivo_ctid_ng/core/exceptions.py
+++ xivo_ctid_ng/core/exceptions.py
@@ -3,36 +3,13 @@
 # SPDX-License-Identifier: GPL-3.0+
 
 import logging
-import time
 
-from functools import wraps
+from xivo import rest_api_helpers
 
 logger = logging.getLogger(__name__)
 
 
-class APIException(Exception):
-    def __init__(self, status_code, message, error_id, details=None):
-        self.status_code = status_code
-        self.message = message
-        self.id_ = error_id
-        self.details = details or {}
-
-
-def handle_api_exception(func):
-    @wraps(func)
-    def wrapper(*args, **kwargs):
-        try:
-            return func(*args, **kwargs)
-        except APIException as error:
-            response = {
-                'message': error.message,
-                'error_id': error.id_,
-                'details': error.details,
-                'timestamp': time.time()
-            }
-            logger.error('%s: %s', error.message, error.details)
-            return response, error.status_code
-    return wrapper
+APIException = rest_api_helpers.APIException
 
 
 class ARIUnreachable(Exception):
