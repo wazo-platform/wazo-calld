0a60d6c8836482d0196ba930fce1f74a9344b8ac
Sebastien Duthil <sduthil@proformatique.com>
2016-02-17 13:46:02 -0500

    integration tests: restart xivo-ctid-ng during call

--- integration_tests/suite/test_collectd.py
+++ integration_tests/suite/test_collectd.py
@@ -175,3 +175,31 @@ class TestCollectdFirstStasisStart(IntegrationTest):
             }))))
 
         until.assert_(assert_function, tries=3)
+
+
+class TestCollectdCtidNgRestart(IntegrationTest):
+
+    asset = 'basic_rest'
+
+    def setUp(self):
+        super(TestCollectdCtidNgRestart, self).setUp()
+        self.ari.reset()
+        self.confd.reset()
+
+    def test_given_ctid_ng_restarts_during_call_when_stasis_channel_destroyed_then_stat_call_end(self):
+        call_id = new_call_id()
+        self.ari.set_channels(MockChannel(id=call_id))
+        self.bus.listen_events(routing_key='collectd.calls', exchange='collectd')
+        self.stasis.event_stasis_start(channel_id=call_id)
+
+        self.restart_service('ctid-ng')
+        until.true(self.ari.websockets, tries=5)  # wait for xivo-ctid-ng to come back up
+        self.stasis.event_channel_destroyed(channel_id=call_id)
+
+        def assert_ctid_ng_sent_end_call_stat():
+            expected_message = 'PUTVAL [^/]+/calls-{app}.{app_instance}/counter-end .* N:1'
+            expected_message = expected_message.format(app=STASIS_APP_NAME,
+                                                       app_instance=STASIS_APP_INSTANCE_NAME)
+            assert_that(self.bus.events(), has_item(matches_regexp(expected_message)))
+
+        until.assert_(assert_ctid_ng_sent_end_call_stat, tries=5)
