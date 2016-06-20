01e253e21a3d574948e4c8418e8ce9e7845af3d9
Etienne Lessard <elessard@proformatique.com>
2016-06-20 09:43:07 -0400

    get_token_user_uuid: handle xivo-auth errors
    This doesn't happens often, since the get_token_user_uuid_from_request
    function is called just after the ACL check is done. But this can still
    happens (token expired, xivo-auth stopped, network problems, etc).
    
    Still, the "best" solution to this problem would probably be to cache
    the token used when the ACL validation is done, so that only 1 request to
    xivo-auth is required to check the ACL + get the token user UUID,
    instead of 2.

--- xivo_ctid_ng/core/auth.py
+++ xivo_ctid_ng/core/auth.py
@@ -2,17 +2,25 @@
 # Copyright 2015-2016 by Avencall
 # SPDX-License-Identifier: GPL-3.0+
 
+import logging
+
 from flask import request
+from requests import HTTPError
 from xivo import auth_verifier
 
 from xivo_ctid_ng.core.exceptions import TokenWithUserUUIDRequiredError
 
+logger = logging.getLogger(__name__)
 required_acl = auth_verifier.required_acl
 
 
 def get_token_user_uuid_from_request(auth_client):
     token = request.headers['X-Auth-Token']
-    token_infos = auth_client.token.get(token)
+    try:
+        token_infos = auth_client.token.get(token)
+    except HTTPError as e:
+        logger.warning('HTTP error from xivo-auth while getting token: %s', e)
+        raise TokenWithUserUUIDRequiredError()
     user_uuid = token_infos['xivo_user_uuid']
     if not user_uuid:
         raise TokenWithUserUUIDRequiredError()
