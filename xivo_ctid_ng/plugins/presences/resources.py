1df7e24c65770cd7a99de7189b0277e059b084c0
Sylvain Boily <sylvainboilydroid@gmail.com>
2016-06-21 22:35:44 -0400

    WIP get presence

--- xivo_ctid_ng/plugins/presences/resources.py
+++ xivo_ctid_ng/plugins/presences/resources.py
@@ -22,6 +22,12 @@ class PresencesResource(AuthResource):
     def __init__(self, presences_service):
         self._presences_service = presences_service
 
+    @required_acl('ctid-ng.users.{user_uuid}.presences.get')
+    def get(self, user_uuid):
+        result = self._presences_service.get_presence(user_uuid)
+
+        return result, 200
+
     @required_acl('ctid-ng.users.{user_uuid}.presences.update')
     def put(self, user_uuid):
         request_body = presence_request_schema.load(request.get_json(force=True)).data
@@ -36,6 +42,13 @@ class UserPresencesResource(AuthResource):
     def __init__(self, auth_client, presences_service):
         self._auth_client = auth_client
         self._presences_service = presences_service
+
+    @required_acl('ctid-ng.users.me.presences.get')
+    def get(self):
+        user_uuid = get_token_user_uuid_from_request(self._auth_client)
+        result = self._presences_service.get_presence(user_uuid)
+
+        return result, 200
 
     @required_acl('ctid-ng.users.me.presences.update')
     def put(self):
--- xivo_ctid_ng/plugins/presences/services.py
+++ xivo_ctid_ng/plugins/presences/services.py
@@ -10,6 +10,13 @@ class PresencesService(object):
     def __init__(self, bus_publisher):
         self._bus_publisher = bus_publisher
 
+    def get_presence(self, user_uuid):
+        presence = { "id": user_uuid,
+                     "origin_uuid": "xivo-uuid",
+                     "presence": "available"
+                   }
+        return presence
+
     def update_presence(self, user_uuid, request_body):
         bus_event = UserStatusUpdateEvent(user_uuid, request_body['status_name'])
         self._bus_publisher.publish(bus_event)
