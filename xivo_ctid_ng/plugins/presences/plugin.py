24bfc967bd2f018f406594ed6de78524cf4a78ac
Sylvain Boily <sylvainboilydroid@gmail.com>
2016-06-27 20:47:18 -0400

    instantiate ctid client in plugin

--- xivo_ctid_ng/plugins/presences/plugin.py
+++ xivo_ctid_ng/plugins/presences/plugin.py
@@ -3,6 +3,7 @@
 # SPDX-License-Identifier: GPL-3.0+
 
 from xivo_auth_client import Client as AuthClient
+from xivo_ctid_client import Client as CtidClient
 
 from .resources import PresencesResource, UserPresencesResource
 from .services import PresencesService
@@ -16,8 +17,9 @@ class Plugin(object):
         config = dependencies['config']
 
         auth_client = AuthClient(**config['auth'])
+        ctid_client = CtidClient(**config['ctid']) 
 
-        presences_service = PresencesService(bus_publisher, config['ctid'])
+        presences_service = PresencesService(bus_publisher, ctid_client, config['ctid'])
 
         api.add_resource(PresencesResource, '/users/<user_uuid>/presences', resource_class_args=[presences_service])
         api.add_resource(UserPresencesResource, '/users/me/presences', resource_class_args=[auth_client, presences_service])
--- xivo_ctid_ng/plugins/presences/services.py
+++ xivo_ctid_ng/plugins/presences/services.py
@@ -4,30 +4,23 @@
 
 from xivo_bus.resources.cti.event import UserStatusUpdateEvent
 
-from contextlib import contextmanager
 import requests
-from xivo_ctid_client import Client as CtidClient
 
 from .exceptions import XiVOCtidUnreachable
 
 
-@contextmanager
-def new_ctid_client(config):
-    yield CtidClient(**config)
-
-
 class PresencesService(object):
 
-    def __init__(self, bus_publisher, ctid_config):
+    def __init__(self, bus_publisher, ctid_client, ctid_config):
         self._bus_publisher = bus_publisher
+        self._ctid_client = ctid_client
         self._ctid_config = ctid_config
 
     def get_presence(self, user_uuid):
-        with new_ctid_client(self._ctid_config) as ctid:
-            try:
-                return ctid.users.get(user_uuid)
-            except requests.RequestException as e:
-                raise XiVOCtidUnreachable(self._ctid_config, e)
+        try:
+            return self._ctid_client.users.get(user_uuid)
+        except requests.RequestException as e:
+            raise XiVOCtidUnreachable(self._ctid_config, e)
 
         return '', 404
 
