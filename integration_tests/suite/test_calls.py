fd347fffc3933838894d20f4da095d44d3b31bb0
Sebastien Duthil <sduthil@proformatique.com>
2016-07-14 10:40:16 -0400

    ignore XIVO_USERUUID on Local channels
    reason: when completing transfers, XIVO_USERUUID is still set to the
    transfer originator on the two Local channels.

--- integration_tests/suite/test_calls.py
+++ integration_tests/suite/test_calls.py
@@ -131,6 +131,23 @@ class TestListCalls(IntegrationTest):
             has_entries({'call_id': 'first-id'}),
             has_entries({'call_id': 'third-id'}))))
 
+    def test_given_local_channels_when_list_then_talking_to_is_none(self):
+        self.ari.set_channels(MockChannel(id='first-id'),
+                              MockChannel(id='second-id', name='Local/second'))
+        self.ari.set_bridges(MockBridge(id='bridge-id', channels=['first-id', 'second-id']))
+        self.confd.set_users(MockUser(uuid='user1-uuid'),
+                             MockUser(uuid='user2-uuid'))
+        self.ari.set_channel_variable({'first-id': {'XIVO_USERUUID': 'user1-uuid'},
+                                       'second-id': {'XIVO_USERUUID': 'user2-uuid'}})
+
+        calls = self.ctid_ng.list_calls()
+
+        assert_that(calls, has_entry('items', contains_inanyorder(
+            has_entries({'call_id': 'first-id',
+                         'talking_to': {'second-id': None}}),
+            has_entries({'call_id': 'second-id',
+                         'talking_to': {'first-id': 'user1-uuid'}}))))
+
 
 class TestUserListCalls(IntegrationTest):
 
