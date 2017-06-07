f7680d36a17705b3d51be1388d46cefdc28f2141
Sebastien Duthil <sduthil@wazo.community>
2017-06-07 09:21:47 -0400

    api spec: extract loading code to lib-python

--- xivo_ctid_ng/plugins/api/resources.py
+++ xivo_ctid_ng/plugins/api/resources.py
@@ -1,14 +1,14 @@
 # -*- coding: utf-8 -*-
-# Copyright 2016 by Avencall
+# Copyright 2016-2017 The Wazo Authors  (see the AUTHORS file)
 # SPDX-License-Identifier: GPL-3.0+
 
-import collections
 import yaml
 import logging
 
 from flask import make_response
 from flask.ext.restful import Resource
-from pkg_resources import resource_string, iter_entry_points
+from xivo.chain_map import ChainMap
+from xivo.rest_api_helpers import load_all_api_specs
 
 logger = logging.getLogger(__name__)
 
@@ -18,24 +18,9 @@ class SwaggerResource(Resource):
     api_filename = "api.yml"
 
     def get(self):
-        api_spec = {}
-        for module in iter_entry_points(group='xivo_ctid_ng.plugins'):
-            try:
-                spec = yaml.load(resource_string(module.module_name, self.api_filename))
-                api_spec = self.update(api_spec, spec)
-            except IOError:
-                logger.debug('API spec for module "%s" does not exist', module.module_name)
+        api_spec = ChainMap(*load_all_api_specs('xivo_ctid_ng.plugins', self.api_filename))
 
         if not api_spec.get('info'):
             return {'error': "API spec does not exist"}, 404
 
-        return make_response(yaml.dump(api_spec), 200, {'Content-Type': 'application/x-yaml'})
-
-    def update(self, a, b):
-        for key, value in b.iteritems():
-            if isinstance(value, collections.Mapping):
-                result = self.update(a.get(key, {}), value)
-                a[key] = result
-            else:
-                a[key] = b[key]
-        return a
+        return make_response(yaml.dump(dict(api_spec)), 200, {'Content-Type': 'application/x-yaml'})
