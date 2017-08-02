fe5271ef5f4e6712fb59acb3cf58361adb4a52e9
Francois Blackburn <fblackburn@wazo.community>
2017-08-02 12:53:04 -0400

    chats: add missing file 'contexts.py'

---
+++ xivo_ctid_ng/plugins/chats/contexts.py
@@ -0,0 +1,21 @@
+# -*- coding: utf-8 -*-
+# Copyright 2017 The Wazo Authors  (see the AUTHORS file)
+# SPDX-License-Identifier: GPL-3.0+
+
+
+class ChatsContexts(object):
+    contexts = {}
+
+    @classmethod
+    def add(cls, key_part1, key_part2, **kwargs):
+        key = cls._build_key(key_part1, key_part2)
+        cls.contexts[key] = kwargs
+
+    @classmethod
+    def get(cls, key_part1, key_part2):
+        key = cls._build_key(key_part1, key_part2)
+        return cls.contexts.get(key)
+
+    @staticmethod
+    def _build_key(part1, part2):
+        return '{}-{}'.format(part1, part2)
