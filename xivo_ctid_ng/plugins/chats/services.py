f9bba152513de208b5e3ea699fc1590c9031ecbb
Francois Blackburn <fblackburn@wazo.community>
2017-08-15 09:27:17 -0400

    chats: add GET /chats

--- xivo_ctid_ng/plugins/chats/services.py
+++ xivo_ctid_ng/plugins/chats/services.py
@@ -3,7 +3,10 @@
 # SPDX-License-Identifier: GPL-3.0+
 
 import requests
+
+from datetime import datetime
 from requests import RequestException
+from requests.utils import quote
 from xivo_ctid_ng.core.exceptions import APIException
 
 
@@ -47,6 +50,88 @@ class ChatsService(object):
         self.mongooseim_config = mongooseim_config
         self.contexts = contexts
 
+    def get_history(self, user_uuid, args):
+        mongooseim_history = self._get_mongooseim_history(user_uuid, args)
+        history = self._convert_history_result(mongooseim_history,
+                                               user_uuid,
+                                               args.get('participant_user_uuid'),
+                                               args.get('participant_server_uuid'))
+
+        return {'items': history}
+
+    def _get_mongooseim_history(self, user_uuid, args):
+        participant_user = args.get('participant_user_uuid')
+        participant_server = args.get('participant_server_uuid', 'localhost')
+        limit = args.get('limit')
+
+        if participant_user:
+            url = self._build_history_url_with_participant(user_uuid, participant_user, participant_server)
+        else:
+            url = self._build_history_url(user_uuid)
+
+        params = {}
+        if limit:
+            params['limit'] = limit
+        try:
+            response = requests.get(url, params=params)
+        except RequestException as e:
+            raise MongooseIMUnreachable(self._xivo_uuid, e)
+
+        if response.status_code != 200:
+            raise MongooseIMException(self._xivo_uuid, response.status_code, response.reason)
+
+        return response.json()
+
+    def _convert_history_result(self, history, user_uuid, participant_user=None, participant_server=None):
+        result = []
+        if participant_server and not participant_user:
+            participant_server = None
+
+        if not participant_server and participant_user:
+            participant_server = self._xivo_uuid
+
+        for entry in history:
+            sender, domain = entry['sender'].split('@', 1)
+            domain = domain if domain != 'localhost' else self._xivo_uuid
+            if sender == user_uuid:
+                direction = 'sent'
+                source_user_uuid = user_uuid
+                source_server_uuid = self._xivo_uuid
+                destination_user_uuid = participant_user
+                destination_server_uuid = participant_server
+            else:
+                direction = 'received'
+                source_user_uuid = sender
+                source_server_uuid = domain
+                destination_user_uuid = user_uuid
+                destination_server_uuid = self._xivo_uuid
+
+            result.append({'msg': entry.get('body'),
+                           'source_user_uuid': source_user_uuid,
+                           'source_server_uuid': source_server_uuid,
+                           'destination_user_uuid': destination_user_uuid,
+                           'destination_server_uuid': destination_server_uuid,
+                           'date': datetime.utcfromtimestamp(int(entry['timestamp'])),
+                           'direction': direction})
+        return result
+
+    def _build_history_url(self, user_uuid):
+        user_jid = self._build_escaped_jid(user_uuid, 'localhost')
+        return 'http://{}:{}/api/messages/{}'.format(self.mongooseim_config['host'],
+                                                     self.mongooseim_config['port'],
+                                                     user_jid)
+
+    def _build_history_url_with_participant(self, user_uuid, participant_user, participant_server):
+        user_jid = self._build_remote_jid(self._xivo_uuid, user_uuid)
+        participant_jid = self._build_remote_jid(participant_server, participant_user)
+        return 'http://{}:{}/api/messages/{}/{}'.format(self.mongooseim_config['host'],
+                                                        self.mongooseim_config['port'],
+                                                        quote(user_jid),
+                                                        quote(participant_jid))
+
+    def _build_escaped_jid(self, username, server):
+        return '{}%40{}'.format(username, server)
+
     def send_message(self, request_body, user_uuid=None):
         _, from_ = self._build_from(request_body, user_uuid)
         to_xivo_uuid, to = self._build_to(request_body)
@@ -84,5 +169,5 @@ class ChatsService(object):
             raise MongooseIMException(self._xivo_uuid, response.status_code, response.reason)
 
     def _build_remote_jid(self, to_domain, to):
-        domain = 'localhost' if to_domain == self._xivo_uuid else to_domain
+        domain = 'localhost' if str(to_domain) == str(self._xivo_uuid) else to_domain
         return '{}@{}'.format(to, domain)
--- xivo_ctid_ng/plugins/chats/tests/test_service.py
+++ xivo_ctid_ng/plugins/chats/tests/test_service.py
@@ -2,11 +2,19 @@
 # Copyright 2016-2017 The Wazo Authors  (see the AUTHORS file)
 # SPDX-License-Identifier: GPL-3.0+
 
+import datetime as dt
 import unittest
 import uuid
 
 from requests import RequestException
-from hamcrest import assert_that, equal_to, raises, calling
+from hamcrest import (
+    assert_that,
+    calling,
+    contains,
+    equal_to,
+    has_entries,
+    raises,
+)
 from mock import Mock, patch
 
 from ..contexts import ChatsContexts
@@ -25,9 +33,9 @@ class TestChatsService(unittest.TestCase):
         self.service = ChatsService(self.xivo_uuid, self.mongooseim_config, ChatsContexts)
         self.alias = 'alice'
         self.msg = 'hello'
-        self.from_ = uuid.uuid4()
-        self.to = uuid.uuid4()
-        self.to_xivo_uuid = uuid.uuid4()
+        self.from_ = str(uuid.uuid4())
+        self.to = str(uuid.uuid4())
+        self.to_xivo_uuid = str(uuid.uuid4())
         self.request_body = {
             'alias': self.alias,
             'from': self.from_,
@@ -68,7 +76,7 @@ class TestChatsService(unittest.TestCase):
         self.service.send_message(self.request_body)
 
         context = ChatsContexts.get(self.from_, self.to)
-        assert_that(context, equal_to({'to_xivo_uuid': str(self.to_xivo_uuid), 'alias': self.alias}))
+        assert_that(context, equal_to({'to_xivo_uuid': self.to_xivo_uuid, 'alias': self.alias}))
 
     @patch('xivo_ctid_ng.plugins.chats.services.requests')
     def test_send_message_without_to_xivo_uuid(self, requests):
@@ -78,7 +86,7 @@ class TestChatsService(unittest.TestCase):
         self.service.send_message(self.request_body)
 
         context = ChatsContexts.get(self.from_, self.to)
-        assert_that(context, equal_to({'to_xivo_uuid': str(self.xivo_uuid), 'alias': self.alias}))
+        assert_that(context, equal_to({'to_xivo_uuid': self.xivo_uuid, 'alias': self.alias}))
 
     @patch('xivo_ctid_ng.plugins.chats.services.requests')
     def test_send_message_without_from(self, requests):
@@ -125,4 +133,90 @@ class TestChatsService(unittest.TestCase):
         requests.post.return_value = Mock(status_code=500)
 
         assert_that(calling(self.service.send_message).with_args(self.request_body),
+                    raises(MongooseIMException))
+
+    def test_convert_history_result_without_participant(self):
+        user2 = str(uuid.uuid4())
+        server2 = str(uuid.uuid4())
+        user3 = str(uuid.uuid4())
+        history = [{'sender': '{}@localhost'.format(self.from_), 'msg': 'hi', 'timestamp': '1502462596'},
+                   {'sender': '{}@{}'.format(user2, server2), 'msg': 'hello', 'timestamp': '1502462597'},
+                   {'sender': '{}@localhost'.format(user3), 'msg': 'hi', 'timestamp': '1502462598'}]
+        result = self.service._convert_history_result(history, self.from_)
+
+        assert_that(result, contains(
+            has_entries(source_user_uuid=self.from_,
+                        source_server_uuid=self.xivo_uuid,
+                        destination_user_uuid=None,
+                        destination_server_uuid=None,
+                        direction='sent',
+                        date=dt.datetime(2017, 8, 11, 14, 43, 16)),
+            has_entries(source_user_uuid=user2,
+                        source_server_uuid=server2,
+                        destination_user_uuid=self.from_,
+                        destination_server_uuid=self.xivo_uuid,
+                        direction='received',
+                        date=dt.datetime(2017, 8, 11, 14, 43, 17)),
+            has_entries(source_user_uuid=user3,
+                        source_server_uuid=self.xivo_uuid,
+                        destination_user_uuid=self.from_,
+                        destination_server_uuid=self.xivo_uuid,
+                        direction='received',
+                        date=dt.datetime(2017, 8, 11, 14, 43, 18)),
+        ))
+
+    def test_convert_history_result_wit_participant_server_only(self):
+        server2 = str(uuid.uuid4())
+        history = [{'sender': '{}@localhost'.format(self.from_), 'msg': 'hi', 'timestamp': '1502462596'}]
+        result = self.service._convert_history_result(history, self.from_, participant_server=server2)
+
+        assert_that(result, contains(
+            has_entries(source_user_uuid=self.from_,
+                        source_server_uuid=self.xivo_uuid,
+                        destination_user_uuid=None,
+                        destination_server_uuid=None,
+                        direction='sent')
+        ))
+
+    def test_convert_history_result_wit_participant_user_only(self):
+        user2 = str(uuid.uuid4())
+        history = [{'sender': '{}@localhost'.format(self.from_), 'msg': 'hi', 'timestamp': '1502462596'}]
+        result = self.service._convert_history_result(history, self.from_, participant_user=user2)
+
+        assert_that(result, contains(
+            has_entries(source_user_uuid=self.from_,
+                        source_server_uuid=self.xivo_uuid,
+                        destination_user_uuid=user2,
+                        destination_server_uuid=self.xivo_uuid,
+                        direction='sent')
+        ))
+
+    def test_convert_history_result_with_participant(self):
+        user2 = str(uuid.uuid4())
+        server2 = str(uuid.uuid4())
+        history = [{'sender': '{}@localhost'.format(self.from_), 'msg': 'hi', 'timestamp': '1502462596'}]
+        result = self.service._convert_history_result(history, self.from_,
+                                                      participant_user=user2,
+                                                      participant_server=server2)
+
+        assert_that(result, contains(
+            has_entries(source_user_uuid=self.from_,
+                        source_server_uuid=self.xivo_uuid,
+                        destination_user_uuid=user2,
+                        destination_server_uuid=server2,
+                        direction='sent')
+        ))
+
+    @patch('xivo_ctid_ng.plugins.chats.services.requests')
+    def test_get_history_when_unreachable_mongooseim(self, requests):
+        requests.get.side_effect = RequestException()
+
+        assert_that(calling(self.service.get_history).with_args(self.from_, {}),
+                    raises(MongooseIMUnreachable))
+
+    @patch('xivo_ctid_ng.plugins.chats.services.requests')
+    def test_get_history_when_mongooseim_return_500(self, requests):
+        requests.get.return_value = Mock(status_code=500)
+
+        assert_that(calling(self.service.get_history).with_args(self.from_, {}),
                     raises(MongooseIMException))
