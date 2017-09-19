74a9c8232e5e4fc200a77734a912958fa00b1e6d
Francois Blackburn <fblackburn@wazo.community>
2017-09-19 08:47:48 -0400

    mongooseim: accept authentication with token
    to allow xmpp connection to mongooseim and also usage of REST API client

---
+++ integration_tests/suite/test_mongooseim.py
@@ -0,0 +1,22 @@
+# -*- coding: utf-8 -*-
+# Copyright 2016-2017 The Wazo Authors  (see the AUTHORS file)
+# SPDX-License-Identifier: GPL-3.0+
+
+import requests
+from hamcrest import assert_that, equal_to
+
+from .test_api.base import IntegrationTest
+
+CTID_NG_VERSION = '1.0'
+BASE_URL = 'http://localhost:{port}/{version}'
+
+
+class TestAuthenticationCoverage(IntegrationTest):
+
+    asset = 'mongooseim'
+
+    def test_auth_on_mongooseim_check_password(self):
+        base_url = BASE_URL.format(port=self.service_port(9501, 'ctid-ng'), version=CTID_NG_VERSION)
+        uri = '{}/mongooseim/authentication/check_password'.format(base_url)
+        result = requests.get(uri, params={'pass': 'invalid-token'})
+        assert_that(result.status_code, equal_to(401))
