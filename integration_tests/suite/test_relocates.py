3d921b8a3ae6ae6607f8dbdfdce4b6aa8bf7c639
Sebastien Duthil <sduthil@wazo.community>
2017-10-16 13:05:22 -0400

    relocates: test invalid user line

--- integration_tests/suite/test_relocates.py
+++ integration_tests/suite/test_relocates.py
@@ -184,6 +184,26 @@ class TestCreateUserRelocate(TestRelocates):
                 'details': has_entries({'user': user_uuid}),
             })))
 
+    def test_given_invalid_line_when_relocate_then_400(self):
+        user_uuid = SOME_USER_UUID
+        line_id = SOME_LINE_ID
+        token = self.given_user_token(user_uuid)
+        relocated_channel_id, initiator_channel_id = self.given_bridged_call_stasis(callee_uuid=user_uuid)
+        self.confd.set_users(MockUser(uuid=user_uuid))
+        ctid_ng = self.make_ctid_ng(token)
+
+        assert_that(
+            calling(ctid_ng.relocates.create_from_user).with_args(
+                initiator_channel_id,
+                'line',
+                {'line_id': line_id}
+            ),
+            raises(CtidNGError).matching(has_properties({
+                'status_code': 400,
+                'error_id': 'relocate-creation-error',
+                'details': has_entries({'line_id': line_id}),
+            })))
+
     def test_given_stasis_channels_a_b_when_b_relocate_to_c_and_answer_then_a_c(self):
         user_uuid = SOME_USER_UUID
         line_id = 12
