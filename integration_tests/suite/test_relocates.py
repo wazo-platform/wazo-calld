7f6d6dbaf37bd7f774db88971d0b94933aab041a
Sebastien Duthil <sduthil@wazo.community>
2017-10-16 13:35:54 -0400

    relocates: test no relocated channel

--- integration_tests/suite/test_relocates.py
+++ integration_tests/suite/test_relocates.py
@@ -223,6 +223,27 @@ class TestCreateUserRelocate(TestRelocates):
                 'details': has_entries({'line_id': line_id}),
             })))
 
+    def test_given_only_one_channel_when_relocate_then_400(self):
+        user_uuid = SOME_USER_UUID
+        line_id = SOME_LINE_ID
+        token = self.given_user_token(user_uuid)
+        initiator_channel = self.stasis_channel()
+        initiator_channel.setChannelVar(variable='XIVO_USERUUID', value=user_uuid)
+        self.confd.set_users(MockUser(uuid=user_uuid, line_ids=[line_id]))
+        self.confd.set_lines(MockLine(id=line_id, name='recipient_autoanswer@local', protocol='local', context=SOME_CONTEXT))
+        ctid_ng = self.make_ctid_ng(token)
+
+        assert_that(
+            calling(ctid_ng.relocates.create_from_user).with_args(
+                initiator_channel.id,
+                'line',
+                {'line_id': line_id}
+            ),
+            raises(CtidNGError).matching(has_properties({
+                'status_code': 400,
+                'error_id': 'relocate-creation-error',
+            })))
+
     def test_given_stasis_channels_a_b_when_b_relocate_to_c_and_answer_then_a_c(self):
         user_uuid = SOME_USER_UUID
         line_id = 12
