8289879528e036ac4b9da59afd6f3257245805d1
Sebastien Duthil <sduthil@wazo.community>
2017-10-16 11:27:17 -0400

    relocates: extract schemas to module

--- xivo_ctid_ng/plugins/relocates/resources.py
+++ xivo_ctid_ng/plugins/relocates/resources.py
@@ -3,52 +3,15 @@
 # SPDX-License-Identifier: GPL-3.0+
 
 from flask import request
-from marshmallow import Schema, fields
-from marshmallow.validate import OneOf, Length, Range
 
 from xivo_ctid_ng.auth import get_token_user_uuid_from_request
 from xivo_ctid_ng.auth import required_acl
 from xivo_ctid_ng.rest_api import AuthResource
 
-
-class LineLocationSchema(Schema):
-    line_id = fields.Integer(validate=Range(min=1), required=True)
-
-
-class LocationField(fields.Field):
-
-    _locations = {
-        'line': fields.Nested(LineLocationSchema),
-    }
-
-    def _deserialize(self, value, attr, data):
-        method = data.get('destination')
-        concrete_location = self._locations.get(method)
-        if not concrete_location:
-            return {}
-        return concrete_location._deserialize(value, attr, data)
-
-
-class UserRelocateRequestSchema(Schema):
-    initiator_call = fields.Str(validate=Length(min=1), required=True)
-    destination = fields.Str(validate=OneOf('line'))
-    location = LocationField(missing=dict)
-
-
-user_relocate_request_schema = UserRelocateRequestSchema(strict=True)
-
-
-class RelocateSchema(Schema):
-    uuid = fields.Str(validate=Length(equal=36), required=True)
-    relocated_call = fields.Str(validate=Length(min=1), required=True, attribute='relocated_channel')
-    initiator_call = fields.Str(validate=Length(min=1), required=True, attribute='initiator_channel')
-    recipient_call = fields.Str(validate=Length(min=1), required=True, attribute='recipient_channel')
-
-    class Meta:
-        strict = True
-
-
-relocate_schema = RelocateSchema()
+from .schema import (
+    relocate_schema,
+    user_relocate_request_schema,
+)
 
 
 class UserRelocatesResource(AuthResource):
