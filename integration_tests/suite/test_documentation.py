df01724ef1900e5be3dd8ccd05dafd0f865e0308
Francois Blackburn <fblackburn@wazo.community>
2017-11-09 08:42:27 -0500

    test swagger: fetch spec before validating
    reason: to avoid timeout

--- integration_tests/suite/test_documentation.py
+++ integration_tests/suite/test_documentation.py
@@ -1,5 +1,5 @@
 # -*- coding: utf-8 -*-
-# Copyright 2016 by Avencall
+# Copyright 2016-2017 The Wazo Authors  (see the AUTHORS file)
 # SPDX-License-Identifier: GPL-3.0+
 
 import requests
@@ -17,11 +17,13 @@ class TestDocumentation(IntegrationTest):
     wait_strategy = NoWaitStrategy()
 
     def test_documentation_errors(self):
-        api_url = 'https://ctid-ng:9500/1.0/api/api.yml'
-        self.validate_api(api_url)
+        ctid_ng_port = self.service_port(9500, 'ctid-ng')
+        api_url = 'https://localhost:{port}/1.0/api/api.yml'.format(port=ctid_ng_port)
+        api = requests.get(api_url, verify=False)
+        self.validate_api(api)
 
-    def validate_api(self, url):
-        port = self.service_port(8080, 'swagger-validator')
-        validator_url = u'http://localhost:{port}/debug'.format(port=port)
-        response = requests.get(validator_url, params={'url': url})
+    def validate_api(self, api):
+        validator_port = self.service_port(8080, 'swagger-validator')
+        validator_url = 'http://localhost:{port}/debug'.format(port=validator_port)
+        response = requests.post(validator_url, data=api)
         assert_that(response.json(), empty(), pprint.pformat(response.json()))
