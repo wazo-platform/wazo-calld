d48aabb009f1c34cf52e957e0fbacedc92fe62e9
Pascal Cadotte Michaud <pcm@wazo.io>
2018-10-18 13:18:46 -0400

    applications: add an event when a DTMF is received

--- integration_tests/suite/helpers/chan_test.py
+++ integration_tests/suite/helpers/chan_test.py
@@ -1,17 +1,25 @@
 # -*- coding: utf-8 -*-
-
-# Copyright 2016-2017 The Wazo Authors  (see the AUTHORS file)
+# Copyright 2016-2018 The Wazo Authors  (see the AUTHORS file)
 # SPDX-License-Identifier: GPL-3.0+
 
 import requests
 
 
 class ChanTest(object):
+
     def __init__(self, ari_config):
         self.config = ari_config
+        self._auth = (self.config['username'], self.config['password'])
 
     def answer_channel(self, channel_id):
         url = '{base}/ari/chan_test/answer'.format(base=self.config['base_url'])
         params = {'id': channel_id}
-        response = requests.post(url, params=params, auth=(self.config['username'], self.config['password']))
+        response = requests.post(url, params=params, auth=self._auth)
+        response.raise_for_status()
+
+    def send_dtmf(self, channel_id, digit):
+        assert len(digit) == 1, 'Only a single digit at a time is supported'
+        url = '{base}/ari/chan_test/dtmf'.format(base=self.config['base_url'])
+        params = {'id': channel_id, 'digit': digit}
+        response = requests.post(url, params=params, auth=self._auth)
         response.raise_for_status()
