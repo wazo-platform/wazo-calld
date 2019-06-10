689ba66f474c09a2d18713a91c770d747184c5fd
Francois Blackburn <fblackburn@wazo.community>
2019-06-10 19:24:00 -0400

    use openapi-spec-validator instead of swagger-validator
    reason: openapi-spec-validator catches more errors

--- integration_tests/suite/test_documentation.py
+++ integration_tests/suite/test_documentation.py
@@ -1,13 +1,17 @@
 # Copyright 2016-2019 The Wazo Authors  (see the AUTHORS file)
 # SPDX-License-Identifier: GPL-3.0-or-later
 
+import logging
 import requests
-import pprint
+import yaml
 
-from hamcrest import assert_that, empty
+from openapi_spec_validator import validate_v2_spec
 
 from .helpers.base import IntegrationTest
 from .helpers.wait_strategy import NoWaitStrategy
+
+logger = logging.getLogger('openapi_spec_validator')
+logger.setLevel(logging.INFO)
 
 
 class TestDocumentation(IntegrationTest):
@@ -16,13 +20,7 @@ class TestDocumentation(IntegrationTest):
     wait_strategy = NoWaitStrategy()
 
     def test_documentation_errors(self):
-        calld_port = self.service_port(9500, 'calld')
-        api_url = 'https://localhost:{port}/1.0/api/api.yml'.format(port=calld_port)
+        port = self.service_port(9500, 'calld')
+        api_url = 'https://localhost:{port}/1.0/api/api.yml'.format(port=port)
         api = requests.get(api_url, verify=False)
-        self.validate_api(api)
-
-    def validate_api(self, api):
-        validator_port = self.service_port(8080, 'swagger-validator')
-        validator_url = 'http://localhost:{port}/debug'.format(port=validator_port)
-        response = requests.post(validator_url, data=api)
-        assert_that(response.json(), empty(), pprint.pformat(response.json()))
+        validate_v2_spec(yaml.safe_load(api.text))
