2e4a3a07cba3e050a2e3de3d828de3d2fd8b480c
Pascal Cadotte Michaud <pcm@wazo.io>
2019-06-28 10:51:39 -0400

    application: add unittests on the stasis_start handler

---
+++ wazo_calld/plugins/applications/tests/test_stasis.py
@@ -0,0 +1,87 @@
+# Copyright 2019 The Wazo Authors  (see the AUTHORS file)
+# SPDX-License-Identifier: GPL-3.0-or-later
+
+from unittest import TestCase
+
+from hamcrest import (
+    assert_that,
+    calling,
+    not_,
+)
+from mock import (
+    Mock,
+    patch,
+    sentinel as s,
+)
+from xivo_test_helpers.hamcrest.raises import raises
+
+from ..stasis import ApplicationStasis
+
+
+class TestApplicationStasisStartHandler(TestCase):
+
+    def setUp(self):
+        self.ari = Mock()
+        self.confd = Mock()
+        self.service = Mock()
+        self.notifier = Mock()
+
+        self.app = ApplicationStasis(self.ari, self.confd, self.service, self.notifier)
+
+    def test_stasis_start_no_a_wazo_app(self):
+        event = {'application': 'foobar'}
+
+        assert_that(
+            calling(self.app.stasis_start).with_args(s.event_object, event),
+            not_(raises(Exception)),
+        )
+        self.app.stasis_start(s.event_object, event)
+
+    def test_stasis_start_user_outgoing_call(self):
+        uuid = 'e3f9b7ef-3fa7-4240-88f1-e6f5c0945b9b'
+        event = {
+            'application': 'wazo-app-{}'.format(uuid),
+            'args': [],
+        }
+
+        with patch.object(self.app, '_stasis_start_user') as fn:
+            self.app.stasis_start(s.event_object, event)
+
+            fn.assert_called_once_with(uuid, s.event_object, event)
+
+    def test_stasis_start_incoming_call(self):
+        uuid = 'e3f9b7ef-3fa7-4240-88f1-e6f5c0945b9b'
+        event = {
+            'application': 'wazo-app-{}'.format(uuid),
+            'args': ['incoming'],
+        }
+
+        with patch.object(self.app, '_stasis_start_incoming') as fn:
+            self.app.stasis_start(s.event_object, event)
+
+            fn.assert_called_once_with(uuid, s.event_object, event)
+
+    def test_stasis_start_originate_in_node(self):
+        uuid = 'e3f9b7ef-3fa7-4240-88f1-e6f5c0945b9b'
+        node_uuid = 'a39a50e5-b5dc-4816-ab6a-1c5b9305e513'
+        event = {
+            'application': 'wazo-app-{}'.format(uuid),
+            'args': ['originate', node_uuid],
+        }
+
+        with patch.object(self.app, '_stasis_start_originate') as fn:
+            self.app.stasis_start(s.event_object, event)
+
+            fn.assert_called_once_with(uuid, node_uuid, s.event_object, event)
+
+    def test_stasis_start_originate_no_node(self):
+        uuid = 'e3f9b7ef-3fa7-4240-88f1-e6f5c0945b9b'
+        event = {
+            'application': 'wazo-app-{}'.format(uuid),
+            'args': ['originate']
+        }
+
+        with patch.object(self.app, '_stasis_start_originate') as fn:
+            self.app.stasis_start(s.event_object, event)
+
+            fn.assert_called_once_with(uuid, None, s.event_object, event)
