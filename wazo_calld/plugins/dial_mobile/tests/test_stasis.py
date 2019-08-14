b63626ae0bfe0a2df13f5a39a013555b40d83b33
Pascal Cadotte Michaud <pcm@wazo.io>
2019-08-14 14:02:15 -0400

    dial_mobile: start adding stasis tests

---
+++ wazo_calld/plugins/dial_mobile/tests/test_stasis.py
@@ -0,0 +1,34 @@
+# Copyright 2019 The Wazo Authors  (see the AUTHORS file)
+# SPDX-License-Identifier: GPL-3.0-or-later
+
+from unittest import TestCase
+
+from mock import Mock
+from hamcrest import (
+    assert_that,
+    calling,
+    not_,
+)
+from xivo_test_helpers.hamcrest.raises import raises
+
+from ..stasis import DialMobileStasis
+
+
+class TestStasisStart(TestCase):
+
+    def setUp(self):
+        self.core_ari = Mock()
+        self.ari = Mock()
+        self.core_ari.client = self.ari
+        self.service = Mock()
+
+        self.stasis = DialMobileStasis(self.ari, self.service)
+
+    def test_other_application(self):
+        assert_that(
+            calling(self.stasis.stasis_start).with_args(Mock(), {'application': 'foobar'}),
+            not_(raises(Exception)),
+        )
+
+        self.service.dial_all_contacts.assert_not_called()
+        self.service.join_bridge.assert_not_called()
