c98a4f5d44eea257e0881a0412ce04360026744e
Sebastien Duthil <sduthil@wazo.community>
2019-11-18 14:48:31 -0500

    config: add missing CORS header

--- wazo_calld/config.py
+++ wazo_calld/config.py
@@ -26,7 +26,7 @@ _DEFAULT_CONFIG = {
         'private_key': '/usr/share/xivo-certs/server.key',
         'cors': {
             'enabled': True,
-            'allow_headers': ['Content-Type'],
+            'allow_headers': ['Content-Type', 'X-Auth-Token', 'Wazo-Tenant'],
         },
     },
     'amid': {
