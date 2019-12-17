e1ec3ed63f4bd5be80e5e67ae83ccd3385a7c230
Sebastien Duthil <sduthil@wazo.community>
2019-12-17 10:21:26 -0500

    switchboards: fix cache resource names

--- wazo_calld/plugins/switchboards/plugin.py
+++ wazo_calld/plugins/switchboards/plugin.py
@@ -32,11 +32,11 @@ class Plugin:
         token_changed_subscribe = dependencies['token_changed_subscribe']
 
         confd_client = ConfdClient(**config['confd'])
-        switchboard_get_cache = ConfdClientGetUUIDCacheDecorator(confd_client.switchboards.get, 'switchboard')
+        switchboard_get_cache = ConfdClientGetUUIDCacheDecorator(confd_client.switchboards.get, resource_name='switchboard')
         confd_client.switchboards.get = switchboard_get_cache
-        line_get_cache = ConfdClientGetUUIDCacheDecorator(confd_client.lines.get, 'lines')
+        line_get_cache = ConfdClientGetUUIDCacheDecorator(confd_client.lines.get, resource_name='line')
         confd_client.lines.get = line_get_cache
-        user_line_get_cache = ConfdClientUserLineGetCacheDecorator(confd_client.users.get, 'users')
+        user_line_get_cache = ConfdClientUserLineGetCacheDecorator(confd_client.users.get, resource_name='user')
         confd_client.users.get = user_line_get_cache
 
         token_changed_subscribe(confd_client.set_token)
