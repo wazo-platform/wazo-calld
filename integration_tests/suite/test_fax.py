5357bd8f25ff524680e2a4fd7f915e220069d7dd
Francois Blackburn <fblackburn@wazo.community>
2019-12-24 17:27:57 -0500

    fax test: use calld_client instead make_calld()

--- integration_tests/suite/test_fax.py
+++ integration_tests/suite/test_fax.py
@@ -23,7 +23,7 @@ from .helpers.confd import (
     MockLine,
     MockUser,
 )
-from .helpers.constants import ASSET_ROOT
+from .helpers.constants import ASSET_ROOT, VALID_TOKEN
 
 
 class TestFax(RealAsteriskIntegrationTest):
@@ -33,6 +33,7 @@ class TestFax(RealAsteriskIntegrationTest):
     def setUp(self):
         super().setUp()
         self.confd.reset()
+        self.calld_client.set_token(VALID_TOKEN)
 
     def _fax_channels(self):
         channels = self.ari.channels.list()
@@ -40,9 +41,8 @@ class TestFax(RealAsteriskIntegrationTest):
         return fax_channels
 
     def test_send_fax_wrong_params(self):
-        calld = self.make_calld()
         assert_that(
-            calling(calld.faxes.send).with_args(
+            calling(self.calld_client.faxes.send).with_args(
                 fax_content='',
                 context=None,
                 extension=None,
@@ -52,9 +52,8 @@ class TestFax(RealAsteriskIntegrationTest):
             })))
 
     def test_send_fax_wrong_extension(self):
-        calld = self.make_calld()
         assert_that(
-            calling(calld.faxes.send).with_args(
+            calling(self.calld_client.faxes.send).with_args(
                 fax_content='',
                 context='recipient',
                 extension='not-found',
@@ -70,10 +69,9 @@ class TestFax(RealAsteriskIntegrationTest):
         )
 
     def test_send_fax_no_amid(self):
-        calld = self.make_calld()
         with self.amid_stopped():
             assert_that(
-                calling(calld.faxes.send).with_args(
+                calling(self.calld_client.faxes.send).with_args(
                     fax_content='',
                     context='recipient',
                     extension='recipient-fax',
@@ -85,10 +83,9 @@ class TestFax(RealAsteriskIntegrationTest):
             )
 
     def test_send_fax_no_ari(self):
-        calld = self.make_calld()
         with self.ari_stopped():
             assert_that(
-                calling(calld.faxes.send).with_args(
+                calling(self.calld_client.faxes.send).with_args(
                     fax_content='',
                     context='recipient',
                     extension='recipient-fax',
@@ -100,14 +97,13 @@ class TestFax(RealAsteriskIntegrationTest):
             )
 
     def test_send_fax_pdf_conversion_failed(self):
-        calld = self.make_calld()
 
         # fax-failure = 1024 zeros
         with open(os.path.join(ASSET_ROOT, 'fax', 'fax-failure.pdf'), 'rb') as fax_file:
             fax_content = fax_file.read()
 
         assert_that(
-            calling(calld.faxes.send).with_args(
+            calling(self.calld_client.faxes.send).with_args(
                 fax_content,
                 context='recipient',
                 extension='recipient-fax',
@@ -119,15 +115,14 @@ class TestFax(RealAsteriskIntegrationTest):
         )
 
     def test_send_fax_pdf(self):
-        calld = self.make_calld()
 
         with open(os.path.join(ASSET_ROOT, 'fax', 'fax.pdf'), 'rb') as fax_file:
             fax_content = fax_file.read()
 
-        fax = calld.faxes.send(fax_content,
-                               context='recipient',
-                               extension='recipient-fax',
-                               caller_id='fax wait')
+        fax = self.calld_client.faxes.send(fax_content,
+                                           context='recipient',
+                                           extension='recipient-fax',
+                                           caller_id='fax wait')
 
         assert_that(fax, has_entries({
             'id': is_not(empty()),
@@ -142,17 +137,15 @@ class TestFax(RealAsteriskIntegrationTest):
         until.assert_(one_fax_channel, timeout=3)
 
     def test_send_fax_events_success(self):
-        calld = self.make_calld()
-
         with open(os.path.join(ASSET_ROOT, 'fax', 'fax.pdf'), 'rb') as fax_file:
             fax_content = fax_file.read()
 
         events = self.bus.accumulator('faxes.outbound.#')
 
-        result = calld.faxes.send(fax_content,
-                                  context='recipient',
-                                  extension='recipient-fax',
-                                  caller_id='fax success')
+        result = self.calld_client.faxes.send(fax_content,
+                                              context='recipient',
+                                              extension='recipient-fax',
+                                              caller_id='fax success')
         fax_id = result['id']
 
         def bus_events_received():
@@ -178,17 +171,15 @@ class TestFax(RealAsteriskIntegrationTest):
         until.assert_(bus_events_received, timeout=3)
 
     def test_send_fax_events_failure(self):
-        calld = self.make_calld()
-
         with open(os.path.join(ASSET_ROOT, 'fax', 'fax.pdf'), 'rb') as fax_file:
             fax_content = fax_file.read()
 
         events = self.bus.accumulator('faxes.outbound.#')
 
-        result = calld.faxes.send(fax_content,
-                                  context='recipient',
-                                  extension='recipient-fax',
-                                  caller_id='fax fail')
+        result = self.calld_client.faxes.send(fax_content,
+                                              context='recipient',
+                                              extension='recipient-fax',
+                                              caller_id='fax fail')
         fax_id = result['id']
 
         def bus_events_received():
@@ -218,13 +209,13 @@ class TestFax(RealAsteriskIntegrationTest):
         user_uuid = 'some-user-id'
         token = 'my-user-token'
         self.auth.set_token(MockUserToken(token, user_uuid=user_uuid, tenant_uuid='my-tenant'))
+        self.calld_client.set_token(token)
 
-        calld = self.make_calld(token)
         with open(os.path.join(ASSET_ROOT, 'fax', 'fax.pdf'), 'rb') as fax_file:
             fax_content = fax_file.read()
 
         assert_that(
-            calling(calld.faxes.send_from_user).with_args(
+            calling(self.calld_client.faxes.send_from_user).with_args(
                 fax_content,
                 extension='recipient-fax',
                 caller_id='fax wait'
@@ -239,13 +230,13 @@ class TestFax(RealAsteriskIntegrationTest):
         token = 'my-user-token'
         self.auth.set_token(MockUserToken(token, user_uuid=user_uuid, tenant_uuid='my-tenant'))
         self.confd.set_users(MockUser(uuid=user_uuid, line_ids=[]))
+        self.calld_client.set_token(token)
 
-        calld = self.make_calld(token)
         with open(os.path.join(ASSET_ROOT, 'fax', 'fax.pdf'), 'rb') as fax_file:
             fax_content = fax_file.read()
 
         assert_that(
-            calling(calld.faxes.send_from_user).with_args(
+            calling(self.calld_client.faxes.send_from_user).with_args(
                 fax_content,
                 extension='recipient-fax',
                 caller_id='fax wait'
@@ -262,15 +253,15 @@ class TestFax(RealAsteriskIntegrationTest):
         self.auth.set_token(MockUserToken(token, user_uuid=user_uuid, tenant_uuid='my-tenant'))
         self.confd.set_users(MockUser(uuid=user_uuid, line_ids=['some-line-id']))
         self.confd.set_lines(MockLine(id='some-line-id', name='line-name', protocol='pjsip', context=context))
+        self.calld_client.set_token(token)
 
-        calld = self.make_calld(token)
         with open(os.path.join(ASSET_ROOT, 'fax', 'fax.pdf'), 'rb') as fax_file:
             fax_content = fax_file.read()
 
         try:
-            calld.faxes.send_from_user(fax_content,
-                                       extension='recipient-fax',
-                                       caller_id='fax wait')
+            self.calld_client.faxes.send_from_user(fax_content,
+                                                   extension='recipient-fax',
+                                                   caller_id='fax wait')
         except Exception as e:
             raise AssertionError('Sending fax raised an exception: {}'.format(e))
 
@@ -286,16 +277,16 @@ class TestFax(RealAsteriskIntegrationTest):
         self.auth.set_token(MockUserToken(token, user_uuid=user_uuid, tenant_uuid='my-tenant'))
         self.confd.set_users(MockUser(uuid=user_uuid, line_ids=['some-line-id']))
         self.confd.set_lines(MockLine(id='some-line-id', name='line-name', protocol='pjsip', context=context))
+        self.calld_client.set_token(token)
 
-        calld = self.make_calld(token)
         with open(os.path.join(ASSET_ROOT, 'fax', 'fax.pdf'), 'rb') as fax_file:
             fax_content = fax_file.read()
 
         events = self.bus.accumulator('faxes.outbound.users.some-user-id.#')
 
-        result = calld.faxes.send_from_user(fax_content,
-                                            extension='recipient-fax',
-                                            caller_id='fax success')
+        result = self.calld_client.faxes.send_from_user(fax_content,
+                                                        extension='recipient-fax',
+                                                        caller_id='fax success')
         fax_id = result['id']
 
         def bus_events_received():
