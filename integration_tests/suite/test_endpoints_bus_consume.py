eb5ee2452354066ab18180fa2f7e6142d48ff0b1
Pascal Cadotte Michaud <pcm@wazo.io>
2020-01-07 11:08:32 -0500

    endpoints: add bus consume test for line status

--- integration_tests/suite/test_endpoints_bus_consume.py
+++ integration_tests/suite/test_endpoints_bus_consume.py
@@ -1,4 +1,4 @@
-# Copyright 2019 The Wazo Authors  (see the AUTHORS file)
+# Copyright 2019-2020 The Wazo Authors  (see the AUTHORS file)
 # SPDX-License-Identifier: GPL-3.0-or-later
 
 from hamcrest import assert_that
@@ -11,10 +11,10 @@ from .helpers.calld import new_call_id
 from .helpers.constants import XIVO_UUID
 from .helpers.wait_strategy import CalldEverythingOkWaitStrategy
 from .helpers.ari_ import MockEndpoint
-from .helpers.confd import MockTrunk
+from .helpers.confd import MockLine, MockTrunk
 
 
-class TestBusConsume(IntegrationTest):
+class TestTrunkBusConsume(IntegrationTest):
 
     asset = 'basic_rest'
     wait_strategy = CalldEverythingOkWaitStrategy()
@@ -247,6 +247,152 @@ class TestBusConsume(IntegrationTest):
                 name='trunk_status_updated',
                 origin_uuid=XIVO_UUID,
                 data=has_entries(id=trunk_id),
+            )))
+
+        until.assert_(assert_function, tries=5)
+
+
+class TestLineBusConsume(IntegrationTest):
+
+    asset = 'basic_rest'
+    wait_strategy = CalldEverythingOkWaitStrategy()
+
+    def setUp(self):
+        super().setUp()
+        self.amid.reset()
+        self.ari.reset()
+        self.confd.reset()
+
+    def test_when_ami_hangup_then_bus_event(self):
+        call_id = new_call_id()
+        line_id = 42
+        tenant_uuid = 'the_tenant_uuid'
+        name = 'abcdef'
+
+        self.ari.set_endpoints(
+            MockEndpoint('PJSIP', name, 'online', channel_ids=[call_id, new_call_id(leap=1)])
+        )
+        self.confd.set_lines(
+            MockLine(
+                line_id,
+                name=name,
+                protocol='sip',
+                endpoint_sip={'name': name, 'username': 'the-username'},
+                tenant_uuid=tenant_uuid,
+            )
+        )
+
+        self.restart_service('calld')
+
+        events = self.bus.accumulator(routing_key='lines.{}.status.updated'.format(line_id))
+        self.reset_clients()
+        self.wait_strategy.wait(self)
+        self.bus.send_ami_hangup_event(call_id, channel='PJSIP/abcdef-00000001')
+
+        def assert_function():
+            assert_that(events.accumulate(), has_item(has_entries(
+                name='line_status_updated',
+                origin_uuid=XIVO_UUID,
+                data=has_entries(
+                    id=line_id,
+                    technology='sip',
+                    name='abcdef',
+                    current_call_count=1,
+                )
+            )))
+
+        until.assert_(assert_function, tries=5)
+
+    def test_when_ami_newchannel_then_bus_event(self):
+        line_id = 42
+        tenant_uuid = 'the_tenant_uuid'
+        name = 'abcdef'
+
+        self.ari.set_endpoints(
+            MockEndpoint('PJSIP', name, 'online', channel_ids=[new_call_id(), new_call_id(leap=1)])
+        )
+        self.confd.set_lines(
+            MockLine(
+                line_id,
+                name=name,
+                protocol='sip',
+                endpoint_sip={'name': name, 'username': 'the-username'},
+                tenant_uuid=tenant_uuid,
+            )
+        )
+
+        self.restart_service('calld')
+
+        events = self.bus.accumulator(routing_key='lines.{}.status.updated'.format(line_id))
+        self.reset_clients()
+        self.wait_strategy.wait(self)
+        self.bus.send_ami_newchannel_event(new_call_id(leap=2), channel='PJSIP/abcdef-00000001')
+
+        def assert_function():
+            assert_that(events.accumulate(), has_item(has_entries(
+                name='line_status_updated',
+                origin_uuid=XIVO_UUID,
+                data=has_entries(
+                    id=line_id,
+                    technology='sip',
+                    name='abcdef',
+                    current_call_count=3,
+                )
+            )))
+
+        until.assert_(assert_function, tries=5)
+
+    def test_when_ami_peerstatus_then_bus_event(self):
+        line_id = 42
+        tenant_uuid = 'the_tenant_uuid'
+        name = 'abcdef'
+
+        self.ari.set_endpoints(
+            MockEndpoint('PJSIP', name, 'offline'),
+        )
+        self.confd.set_lines(
+            MockLine(
+                line_id,
+                name=name,
+                protocol='sip',
+                endpoint_sip={'name': name, 'username': 'the-username'},
+                tenant_uuid=tenant_uuid,
+            )
+        )
+
+        self.restart_service('calld')
+
+        events = self.bus.accumulator(routing_key='lines.{}.status.updated'.format(line_id))
+        self.reset_clients()
+        self.wait_strategy.wait(self)
+        self.bus.send_ami_peerstatus_event('PJSIP', 'PJSIP/abcdef', 'Reachable')
+
+        def assert_function():
+            assert_that(events.accumulate(), has_item(has_entries(
+                name='line_status_updated',
+                origin_uuid=XIVO_UUID,
+                data=has_entries(
+                    id=line_id,
+                    technology='sip',
+                    name='abcdef',
+                    registered=True,
+                )
+            )))
+
+        until.assert_(assert_function, tries=5)
+
+        self.bus.send_ami_peerstatus_event('PJSIP', 'PJSIP/abcdef', 'Unreachable')
+
+        def assert_function():
+            assert_that(events.accumulate(), has_item(has_entries(
+                name='line_status_updated',
+                origin_uuid=XIVO_UUID,
+                data=has_entries(
+                    id=line_id,
+                    technology='sip',
+                    name='abcdef',
+                    registered=False,
+                )
             )))
 
         until.assert_(assert_function, tries=5)
