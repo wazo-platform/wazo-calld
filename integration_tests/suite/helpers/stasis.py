dee23ac6efaa7dcbf15b4b49989bd0d413aa87a2
Sebastien Duthil <sduthil@wazo.community>
2020-08-12 16:00:25 -0400

    tests: remove constants that should not be shared

--- integration_tests/suite/helpers/stasis.py
+++ integration_tests/suite/helpers/stasis.py
@@ -1,13 +1,11 @@
-# Copyright 2015-2018 The Wazo Authors  (see the AUTHORS file)
+# Copyright 2015-2020 The Wazo Authors  (see the AUTHORS file)
 # SPDX-License-Identifier: GPL-3.0-or-later
 
 import requests
 
 from hamcrest import assert_that, equal_to
 
-from .constants import STASIS_APP_ARGS
-from .constants import STASIS_APP_INSTANCE_NAME
-from .constants import STASIS_APP_NAME
+from .constants import SOME_STASIS_APP
 
 
 class StasisClient:
@@ -21,12 +19,12 @@ class StasisClient:
                                                     port=self._port,
                                                     path='/'.join(parts))
 
-    def event_answer_connect(self, from_, new_call_id):
+    def event_answer_connect(self, from_, new_call_id, stasis_app, stasis_app_instance):
         url = self.url('_send_ws_event')
         body = {
-            "application": STASIS_APP_NAME,
+            "application": stasis_app,
             "args": [
-                STASIS_APP_INSTANCE_NAME,
+                stasis_app_instance,
                 "dialed_from",
                 from_
             ],
@@ -61,7 +59,7 @@ class StasisClient:
     def event_hangup(self, channel_id):
         url = self.url('_send_ws_event')
         body = {
-            "application": STASIS_APP_NAME,
+            "application": SOME_STASIS_APP,
             "channel": {
                 "accountcode": "code",
                 "caller": {
@@ -90,76 +88,12 @@ class StasisClient:
         response = requests.post(url, json=body)
         assert_that(response.status_code, equal_to(201))
 
-    def event_new_channel(self, channel_id):
+    def event_stasis_start(self, channel_id, stasis_app, stasis_app_instance, stasis_args=None):
+        stasis_args = stasis_args or []
         url = self.url('_send_ws_event')
         body = {
-            "application": STASIS_APP_NAME,
-            "args": [],
-            "channel": {
-                "accountcode": "",
-                "caller": {
-                    "name": "my-name",
-                    "number": "my-number"
-                },
-                "connected": {
-                    "name": "",
-                    "number": ""
-                },
-                "creationtime": "2015-12-16T15:13:59.526-0500",
-                "dialplan": {
-                    "context": "default",
-                    "exten": "",
-                    "priority": 1
-                },
-                "id": channel_id,
-                "language": "en_US",
-                "name": "PJSIP/my-sip-00000020",
-                "state": "Up"
-            },
-            "timestamp": "2015-12-16T15:14:04.269-0500",
-            "type": "StasisStart"
-        }
-
-        response = requests.post(url, json=body)
-        assert_that(response.status_code, equal_to(201))
-
-    def event_channel_updated(self, channel_id, state='Ring'):
-        url = self.url('_send_ws_event')
-        body = {
-            "application": STASIS_APP_NAME,
-            "channel": {
-                "accountcode": "code",
-                "caller": {
-                    "name": "my-name",
-                    "number": "my-number"
-                },
-                "connected": {
-                    "name": "",
-                    "number": ""
-                },
-                "creationtime": "2015-12-18T15:40:32.439-0500",
-                "dialplan": {
-                    "context": "default",
-                    "exten": "my-exten",
-                    "priority": 1
-                },
-                "id": channel_id,
-                "language": "fr_FR",
-                "name": "my-name",
-                "state": state
-            },
-            "timestamp": "2015-12-18T15:40:39.073-0500",
-            "type": "ChannelStateChange"
-        }
-
-        response = requests.post(url, json=body)
-        assert_that(response.status_code, equal_to(201))
-
-    def event_stasis_start(self, channel_id, stasis_args=STASIS_APP_ARGS):
-        url = self.url('_send_ws_event')
-        body = {
-            "application": STASIS_APP_NAME,
-            "args": stasis_args,
+            "application": stasis_app,
+            "args": [stasis_app_instance] + stasis_args,
             "channel": {
                 "accountcode": "code",
                 "caller": {
@@ -188,12 +122,12 @@ class StasisClient:
         response = requests.post(url, json=body)
         assert_that(response.status_code, equal_to(201))
 
-    def event_channel_destroyed(self, channel_id, creation_time=None, timestamp=None, connected_number=''):
+    def event_channel_destroyed(self, channel_id, stasis_app, creation_time=None, timestamp=None, connected_number=''):
         url = self.url('_send_ws_event')
         creation_time = creation_time or "2016-02-04T15:10:21.225-0500"
         timestamp = timestamp or "2016-02-04T15:10:22.548-0500"
         body = {
-            "application": STASIS_APP_NAME,
+            "application": stasis_app,
             "cause": 0,
             "cause_txt": "Unknown",
             "channel": {
