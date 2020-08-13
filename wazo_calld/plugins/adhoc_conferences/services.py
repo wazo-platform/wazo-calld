1a14f54df8fdceff78a56b271921261e13727148
Sebastien Duthil <sduthil@wazo.community>
2020-08-13 13:52:37 -0400

    adhoc conferences: refactor channel exist check

--- wazo_calld/plugins/adhoc_conferences/services.py
+++ wazo_calld/plugins/adhoc_conferences/services.py
@@ -1,7 +1,6 @@
 # Copyright 2020 The Wazo Authors  (see the AUTHORS file)
 # SPDX-License-Identifier: GPL-3.0-or-later
 
-from ari.exceptions import ARINotFound
 from wazo_calld.plugin_helpers.ari_ import Channel
 
 from .exceptions import (
@@ -17,17 +16,13 @@ class AdhocConferencesService:
         self._ari = ari
 
     def create_from_user(self, host_call_id, participant_call_ids, user_uuid):
-        try:
-            host_channel = self._ari.channels.get(channelId=host_call_id)
-        except ARINotFound:
+        host_channel = Channel(host_call_id, self._ari)
+        if not host_channel.exists():
             raise HostCallNotFound(host_call_id)
 
         for participant_call_id in participant_call_ids:
-            try:
-                participant_channel = self._ari.channels.get(channelId=participant_call_id)
-            except ARINotFound:
+            if not Channel(participant_call_id, self._ari).exists():
                 raise ParticipantCallNotFound(host_call_id)
 
-        host_call_user = Channel(host_call_id, self._ari).user()
-        if host_call_user != user_uuid:
-            raise HostPermissionDenied(host_call_id, user_uuid)
+        if host_channel.user() != user_uuid:
+            raise HostPermissionDenied(host_call_id, host_channel.user())
