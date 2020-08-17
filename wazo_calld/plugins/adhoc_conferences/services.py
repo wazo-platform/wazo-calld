686ff17c47061423781a65e6719a95e739047427
Sebastien Duthil <sduthil@wazo.community>
2020-08-17 14:06:24 -0400

    adhoc conferences: extract long methods

--- wazo_calld/plugins/adhoc_conferences/services.py
+++ wazo_calld/plugins/adhoc_conferences/services.py
@@ -43,11 +43,35 @@ class AdhocConferencesService:
         adhoc_conference_id = str(uuid.uuid4())
         logger.debug('creating adhoc conference %s', adhoc_conference_id)
 
+        logger.debug('adhoc conference %s: looking for peer of host %s', adhoc_conference_id, host_call_id)
+        host_peer_channel_id = self._find_call_peer(host_call_id)
+
+        self._redirect_host(host_call_id, host_peer_channel_id, adhoc_conference_id)
+
+        remaining_participant_call_ids = set(participant_call_ids) - {host_peer_channel_id}
+        logger.debug('adhoc conference %s: remaining participants %s', adhoc_conference_id, remaining_participant_call_ids)
+        for participant_call_id in remaining_participant_call_ids:
+            logger.debug('adhoc conference %s: looking for peer of participant %s', adhoc_conference_id, participant_call_id)
+            discarded_host_channel_id = self._find_call_peer(participant_call_id)
+
+            logger.debug('adhoc conference %s: processing participant %s and peer %s', adhoc_conference_id, participant_call_id, discarded_host_channel_id)
+            self._redirect_participant(participant_call_id, discarded_host_channel_id, adhoc_conference_id)
+
+        return {
+            'conference_id': adhoc_conference_id,
+        }
+
+    def _find_call_peer(self, call_id):
+        try:
+            return Channel(call_id, self._ari).only_connected_channel().id
+        except (TooManyChannels, NotEnoughChannels):
+            raise  # replace me
+
+    def _redirect_host(self, host_call_id, host_peer_channel_id, adhoc_conference_id):
         try:
             host_channel = self._ari.channels.get(channelId=host_call_id)
         except ARINotFound:
             raise AdhocConferenceCreationError('host call was hungup')
-        host_peer_channel_id = Channel(host_call_id, self._ari).only_connected_channel().id
         try:
             host_peer_channel = self._ari.channels.get(channelId=host_peer_channel_id)
         except ARINotFound:
@@ -72,46 +96,30 @@ class AdhocConferencesService:
             extra_channel=host_channel.json['name'],
         )
 
-        remaining_participants = set(participant_call_ids) - {host_peer_channel_id}
-        logger.debug('adhoc conference %s: remaining participants %s', adhoc_conference_id, remaining_participants)
-        participant_call_pairs = self._find_participant_pairs(remaining_participants)
-        for participant_channel_id, discarded_host_channel_id in participant_call_pairs:
-            logger.debug('adhoc conference %s: processing participant %s and peer %s', adhoc_conference_id, participant_channel_id, discarded_host_channel_id)
-            try:
-                discarded_host_channel = self._ari.channels.get(channelId=discarded_host_channel_id)
-            except ARINotFound:
-                raise AdhocConferenceCreationError('host call was hungup')
-            try:
-                participant_channel = self._ari.channels.get(channelId=participant_channel_id)
-            except ARINotFound:
-                raise AdhocConferenceCreationError('participant call was hungup')
+    def _redirect_participant(self, participant_channel_id, discarded_host_channel_id, adhoc_conference_id):
+        try:
+            discarded_host_channel = self._ari.channels.get(channelId=discarded_host_channel_id)
+        except ARINotFound:
+            raise AdhocConferenceCreationError('host call was hungup')
+        try:
+            participant_channel = self._ari.channels.get(channelId=participant_channel_id)
+        except ARINotFound:
+            raise AdhocConferenceCreationError('participant call was hungup')
 
-            logger.debug('adhoc conference %s: redirecting participant call %s and discarding peer %s', adhoc_conference_id, participant_channel_id, discarded_host_channel_id)
-            try:
-                participant_channel.setChannelVar(variable='WAZO_ADHOC_CONFERENCE_ID',
-                                                  value=adhoc_conference_id,
-                                                  bypassStasis=True)
-            except ARIException as e:
-                logger.exception('ARI error: %s', e)
-                return
-            ami.redirect(
-                self._amid_client,
-                participant_channel.json['name'],
-                context='convert_to_stasis',
-                exten='adhoc_conference',
-                extra_channel=discarded_host_channel.json['name'],
-                extra_context='convert_to_stasis',
-                extra_exten='h'
-            )
-
-        return {
-            'conference_id': adhoc_conference_id,
-        }
-
-    def _find_participant_pairs(self, participant_call_ids):
-        for participant_call_id in participant_call_ids:
-            try:
-                participant_peer_call_id = Channel(participant_call_id, self._ari).only_connected_channel().id
-            except (TooManyChannels, NotEnoughChannels):
-                raise  # replace me
-            yield (participant_call_id, participant_peer_call_id)
+        logger.debug('adhoc conference %s: redirecting participant call %s and discarding peer %s', adhoc_conference_id, participant_channel_id, discarded_host_channel_id)
+        try:
+            participant_channel.setChannelVar(variable='WAZO_ADHOC_CONFERENCE_ID',
+                                              value=adhoc_conference_id,
+                                              bypassStasis=True)
+        except ARIException as e:
+            logger.exception('ARI error: %s', e)
+            return
+        ami.redirect(
+            self._amid_client,
+            participant_channel.json['name'],
+            context='convert_to_stasis',
+            exten='adhoc_conference',
+            extra_channel=discarded_host_channel.json['name'],
+            extra_context='convert_to_stasis',
+            extra_exten='h'
+        )
