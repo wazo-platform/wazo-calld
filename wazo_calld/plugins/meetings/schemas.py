42af200633c78e6df21078c63b7b7fa20a0fc5ea
Sebastien Duthil <sduthil@wazo.community>
2021-09-21 09:16:33 -0400

    meetings: fix copyrights

--- wazo_calld/plugins/meetings/schemas.py
+++ wazo_calld/plugins/meetings/schemas.py
@@ -1,4 +1,4 @@
-# Copyright 2019-2021 The Wazo Authors  (see the AUTHORS file)
+# Copyright 2021 The Wazo Authors  (see the AUTHORS file)
 # SPDX-License-Identifier: GPL-3.0-or-later
 
 from marshmallow import (
