4e7358c45e8fb54afe7f79a78b7769cde3d2e0f7
Farhad <farhad2161@yahoo.com>
2022-02-05 01:32:47 -0500

    add tests for new option ivr_extension and wait_time in fax api

--- integration_tests/suite/test_fax.py
+++ integration_tests/suite/test_fax.py
@@ -382,3 +382,53 @@ class TestFax(RealAsteriskIntegrationTest):
             ))
 
         until.assert_(bus_events_received, timeout=10)
+
+    def test_send_fax_ivr(self):
+
+        with open(os.path.join(ASSET_ROOT, 'fax', 'fax.pdf'), 'rb') as fax_file:
+            fax_content = fax_file.read()
+
+        fax = self.calld_client.faxes.send(fax_content,
+                                           context='recipient',
+                                           extension='recipient-fax',
+                                           caller_id='fax wait',
+                                           wait_time='1',
+                                           ivr_extension='2')
+
+        assert_that(fax, has_entries({
+            'id': is_not(empty()),
+            'context': 'recipient',
+            'extension': 'recipient-fax',
+            'caller_id': 'fax wait',
+            'wait_time': '1',
+            'ivr_extension': '2',
+        }))
+
+        def one_fax_channel():
+            assert_that(self._fax_channels(), has_length(1))
+
+        until.assert_(one_fax_channel, timeout=3)
+
+    def test_send_fax_ivr_from_user(self):
+        user_uuid = 'some-user-id'
+        context = 'user-context'
+        self.confd.set_users(MockUser(uuid=user_uuid, line_ids=['some-line-id']))
+        self.confd.set_lines(MockLine(id='some-line-id', name='line-name', protocol='pjsip', context=context))
+        calld_client = self.make_user_calld(user_uuid)
+
+        with open(os.path.join(ASSET_ROOT, 'fax', 'fax.pdf'), 'rb') as fax_file:
+            fax_content = fax_file.read()
+
+        try:
+            calld_client.faxes.send_from_user(fax_content,
+                                              extension='recipient-fax',
+                                              caller_id='fax wait',
+                                              wait_time='1',
+                                              ivr_extension='2')
+        except Exception as e:
+            raise AssertionError('Sending fax raised an exception: {}'.format(e))
+
+        def one_fax_channel():
+            assert_that(self._fax_channels(), has_length(1))
+
+        until.assert_(one_fax_channel, timeout=3)
