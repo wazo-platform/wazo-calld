6fd521249f04551cd703f5b9cdf34c4ba74a5bd3
Julien Alie <jalie@wazo.io>
2022-02-24 14:51:33 -0500

    Meetings: Add tenant_uuid to bus events headers

--- wazo_calld/plugins/meetings/notifier.py
+++ wazo_calld/plugins/meetings/notifier.py
@@ -1,4 +1,4 @@
-# Copyright 2021 The Wazo Authors  (see the AUTHORS file)
+# Copyright 2021-2022 The Wazo Authors  (see the AUTHORS file)
 # SPDX-License-Identifier: GPL-3.0-or-later
 
 from xivo_bus.resources.meeting.event import (
@@ -26,23 +26,39 @@ class MeetingsNotifier:
     def __init__(self, bus_producer):
         self._bus_producer = bus_producer
 
+    @staticmethod
+    def _build_headers(user_uuids=None, **kwargs):
+        headers = {}
+        for key, value in kwargs.items():
+            headers[key] = value
+
+        for uuid in user_uuids or []:
+            headers[f'user_uuid:{uuid}'] = True
+
+        return headers
+
     def participant_joined(
         self, tenant_uuid, meeting_uuid, participant, participants_already_present
     ):
         participants = Participants(participant, *participants_already_present)
-        for user_uuid_concerned in participants.valid_user_uuids():
+        participants_uuids = list(participants.valid_user_uuids())
+
+        for user_uuid_concerned in participants_uuids:
             user_event = UserParticipantJoinedMeetingEvent(
                 meeting_uuid, participant, user_uuid_concerned
             )
-            self._bus_producer.publish(user_event)
+            headers = self._build_headers(
+                meeting_uuid=meeting_uuid,
+                tenant_uuid=tenant_uuid,
+                user_uuids=[user_uuid_concerned],
+            )
+            self._bus_producer.publish(user_event, headers=headers)
 
-        headers = {
-            'name': 'meeting_participant_joined',
-            'tenant_uuid': tenant_uuid,
-            'meeting_uuid': meeting_uuid,
-        }
-        for user_uuid_concerned in participants.valid_user_uuids():
-            headers['user_uuid:{}'.format(user_uuid_concerned)] = True
+        headers = self._build_headers(
+            meeting_uuid=meeting_uuid,
+            tenant_uuid=tenant_uuid,
+            user_uuids=participants_uuids,
+        )
         meeting_event = ParticipantJoinedMeetingEvent(meeting_uuid, participant)
         self._bus_producer.publish(meeting_event, headers=headers)
 
@@ -50,18 +66,23 @@ class MeetingsNotifier:
         self, tenant_uuid, meeting_uuid, participant, participants_already_present
     ):
         participants = Participants(participant, *participants_already_present)
-        for user_uuid_concerned in participants.valid_user_uuids():
+        participants_uuids = list(participants.valid_user_uuids())
+
+        for user_uuid_concerned in participants_uuids:
             user_event = UserParticipantLeftMeetingEvent(
                 meeting_uuid, participant, user_uuid_concerned
             )
-            self._bus_producer.publish(user_event)
+            headers = self._build_headers(
+                meeting_uuid=meeting_uuid,
+                tenant_uuid=tenant_uuid,
+                user_uuids=[user_uuid_concerned],
+            )
+            self._bus_producer.publish(user_event, headers=headers)
 
-        headers = {
-            'name': 'meeting_participant_left',
-            'tenant_uuid': tenant_uuid,
-            'meeting_uuid': meeting_uuid,
-        }
-        for user_uuid_concerned in participants.valid_user_uuids():
-            headers['user_uuid:{}'.format(user_uuid_concerned)] = True
+        headers = self._build_headers(
+            meeting_uuid=meeting_uuid,
+            tenant_uuid=tenant_uuid,
+            user_uuids=participants_uuids,
+        )
         meeting_event = ParticipantLeftMeetingEvent(meeting_uuid, participant)
         self._bus_producer.publish(meeting_event, headers=headers)
