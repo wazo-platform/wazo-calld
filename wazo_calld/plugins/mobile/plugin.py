1ce00d264947d1a4ea5f66518d1f1b1b4984885a
Julien Alie <jalie@wazo.io>
2022-02-24 15:58:30 -0500

    Mobile: Add tenant_uuid in bus events headers
    * Also removed ArbitraryEvent in favor of PushNotificationEvent
    in xivo-bus

--- wazo_calld/plugins/mobile/plugin.py
+++ wazo_calld/plugins/mobile/plugin.py
@@ -1,8 +1,9 @@
 # -*- coding: utf-8 -*-
-# Copyright 2018-2019 The Wazo Authors  (see the AUTHORS file)
+# Copyright 2018-2022 The Wazo Authors  (see the AUTHORS file)
 # SPDX-License-Identifier: GPL-3.0+
 
 from .bus_consume import PushNotificationBusEventHandler
+from .notifier import MobilePushNotifier
 
 
 class Plugin(object):
@@ -11,5 +12,7 @@ class Plugin(object):
         bus_consumer = dependencies['bus_consumer']
         bus_publisher = dependencies['bus_publisher']
 
-        push_notification_bus_event_handler = PushNotificationBusEventHandler(bus_publisher)
+        notifier = MobilePushNotifier(bus_publisher)
+
+        push_notification_bus_event_handler = PushNotificationBusEventHandler(notifier)
         push_notification_bus_event_handler.subscribe(bus_consumer)
