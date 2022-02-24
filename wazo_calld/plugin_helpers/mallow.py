01220500eb74001066db4dbe7599ae3491d98356
Francois Blackburn <fblackburn@wazo.community>
2022-02-24 09:36:59 -0500

    make StrictDict compatible with marshmallow 3.10.0

--- wazo_calld/plugin_helpers/mallow.py
+++ wazo_calld/plugin_helpers/mallow.py
@@ -1,4 +1,4 @@
-# Copyright 2016-2018 The Wazo Authors  (see the AUTHORS file)
+# Copyright 2016-2022 The Wazo Authors  (see the AUTHORS file)
 # SPDX-License-Identifier: GPL-3.0-or-later
 
 from marshmallow import fields
@@ -11,12 +11,12 @@ class StrictDict(fields.Dict):
         self.key_field = key_field
         self.value_field = value_field
 
-    def _deserialize(self, value, attr, data):
-        value = super()._deserialize(value, attr, data)
+    def _deserialize(self, value, attr, data, **kwargs):
+        value = super()._deserialize(value, attr, data, **kwargs)
 
         result = {}
         for key, inner_value in value.items():
-            new_key = self.key_field.deserialize(key, attr, data)
-            new_value = self.value_field.deserialize(inner_value, attr, data)
+            new_key = self.key_field.deserialize(key, attr, data, **kwargs)
+            new_value = self.value_field.deserialize(inner_value, attr, data, **kwargs)
             result[new_key] = new_value
         return result
