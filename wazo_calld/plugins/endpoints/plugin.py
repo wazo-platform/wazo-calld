fd288d79a611da6fe3dbe61643406b09b144f1b6
Francois Blackburn <fblackburn@wazo.community>
2023-08-08 10:13:11 -0400

    status: add endpoints plugin status
    why: callback can be long to be executed and can cause some race
    condition with tests

--- wazo_calld/plugins/endpoints/plugin.py
+++ wazo_calld/plugins/endpoints/plugin.py
@@ -3,6 +3,7 @@
 
 from wazo_confd_client import Client as ConfdClient
 from xivo.pubsub import CallbackCollector
+from xivo.status import Status
 
 from .bus import EventHandler
 from .http import LineEndpoints, TrunkEndpoints
@@ -15,6 +16,7 @@ class Plugin:
         api = dependencies['api']
         ari = dependencies['ari']
         config = dependencies['config']
+        status_aggregator = dependencies['status_aggregator']
         token_changed_subscribe = dependencies['token_changed_subscribe']
         bus_consumer = dependencies['bus_consumer']
         bus_publisher = dependencies['bus_publisher']
@@ -28,12 +30,16 @@ class Plugin:
         status_cache = NotifyingStatusCache(notifier.endpoint_updated, ari.client)
         endpoints_service = EndpointsService(confd_cache, status_cache)
 
+        self._async_tasks_completed = False
         startup_callback_collector = CallbackCollector()
         ari.client_initialized_subscribe(startup_callback_collector.new_source())
         startup_callback_collector.subscribe(status_cache.initialize)
+        startup_callback_collector.subscribe(self._set_async_tasks_completed)
 
         event_handler = EventHandler(status_cache, confd_cache)
         event_handler.subscribe(bus_consumer)
+
+        status_aggregator.add_provider(self._provide_status)
 
         api.add_resource(
             TrunkEndpoints,
@@ -50,3 +56,10 @@ class Plugin:
                 endpoints_service,
             ],
         )
+
+    def _set_async_tasks_completed(self):
+        self._async_tasks_completed = True
+
+    def _provide_status(self, status):
+        value = Status.ok if self._async_tasks_completed else Status.fail
+        status['plugins']['endpoints']['status'] = value
