ba921f02cfda08269d518cdd8678d7dddee6e8d6
Julien Alie <jalie@wazo.io>
2024-05-31 15:37:58 -0400

    parkings: add `/parkings` endpoint

--- wazo_calld/plugins/parking_lots/schemas.py
+++ wazo_calld/plugins/parking_lots/schemas.py
@@ -23,12 +23,14 @@ class _Base(Schema):
 
 
 class ParkingLotSchema(_Base):
+    id = Integer(dump_only=True)
+    name = String(dump_only=True)
     slots_start = Integer(dump_only=True)
     slots_end = Integer(dump_only=True)
     slots_total = Integer(dump_only=True, default=0)
     slots_remaining = Integer(dump_only=True, default=0)
     default_timeout = Integer(attribute='timeout', dump_only=True)
-    calls = List(Nested("ParkedCallGetResponseSchema"))
+    calls = List(Nested("ParkedCallGetResponseSchema"), default=list)
 
     @pre_dump
     def calls_from_context(self, obj, **kwargs):
@@ -44,11 +46,24 @@ class ParkingLotSchema(_Base):
         return obj
 
 
+class ParkingLotListSchema(ParkingLotSchema):
+    @pre_dump
+    def unwrap_calls(self, obj, **kwargs):
+        parkinglot, calls = obj
+        parkinglot.calls = calls
+        return parkinglot
+
+
 class ParkedCallGetResponseSchema(_Base):
     call_id = String(attribute='parkee_uniqueid', dump_only=True)
+    caller_id_name = String(attribute='parkee_caller_id_name', dump_only=True)
+    caller_id_num = String(attribute='parkee_caller_id_num', dump_only=True)
+    connected_line_name = String(attribute='parkee_connected_line_name', dump_only=True)
+    connected_line_num = String(attribute='parkee_connected_line_num', dump_only=True)
     slot = String(attribute='parking_space', dump_only=True)
     parked_at = Method('compute_park_time', dump_only=True)
     timeout_at = Method('compute_timeout', allow_none=True, dump_only=True)
+    timeout = Integer(attribute='parking_timeout', dump_only=True)
 
     def compute_park_time(self, parked_call: AsteriskParkedCall) -> str:
         return timestamp_since(parked_call.parking_duration)
--- wazo_calld/plugins/parking_lots/services.py
+++ wazo_calld/plugins/parking_lots/services.py
@@ -27,7 +27,7 @@ from .exceptions import (
     NoSuchParking,
     ParkingFull,
 )
-from .helpers import DONT_CHECK_TENANT, DontCheckTenant
+from .helpers import DONT_CHECK_TENANT, DontCheckTenant, split_parking_id_from_name
 from .notifier import ParkingNotifier
 
 if TYPE_CHECKING:
@@ -40,8 +40,10 @@ if TYPE_CHECKING:
     from .dataclasses_ import ConfdParkingLot
 
 
-RETRIES = 5
+CHECK_PARKING_DELAY_BETWEEN_RETRIES = 0.2
+CHECK_PARKING_MAX_RETRIES = 5
 PARKED_CHANNEL_VAR = 'WAZO_CALL_PARKED'
+
 logger = logging.getLogger(__name__)
 
 
@@ -112,15 +114,30 @@ class ParkingService:
             raise WazoAmidError(self._amid, e)
 
         check_full: bool | None = None
-        for _ in range(RETRIES):
+        for _ in range(CHECK_PARKING_MAX_RETRIES):
             try:
                 return self.get_parked_call(tenant_uuid, parking.id, parkee.id)
             except NoSuchParkedCall:
                 if check_full is None:
                     if check_full := self.is_parking_full(tenant_uuid, parking.id):
                         raise ParkingFull(tenant_uuid, parking.id, parkee.id)
-                sleep(0.2)  # wait 200 ms between attempts
+                sleep(CHECK_PARKING_DELAY_BETWEEN_RETRIES)
         raise NoSuchParkedCall(tenant_uuid, parking.id, parkee.id)
+
+    def count_parked_calls(self, tenant_uuid: str, parking_id: int) -> int:
+        parking = self.get_parking(tenant_uuid, parking_id)
+
+        try:
+            results: list[dict] = self._amid.action(
+                'ParkedCalls', {'ParkingLot': f'parkinglot-{parking.id}'}
+            )
+        except RequestException as e:
+            raise WazoAmidError(self._amid, e)
+
+        complete = results.pop()
+        if complete.get('Event') != 'ParkedCallsComplete':
+            raise WazoAmidError(self._amid, 'missing ParkedCallsComplete message')
+        return int(complete['Total'])
 
     def find_parked_call(
         self, tenant_uuid: str, parking_id: int, call_id: str
@@ -146,6 +163,12 @@ class ParkingService:
             raise NoSuchParking(parking_id)
         return parking
 
+    def is_parking_full(self, tenant_uuid: str, parking_id: int) -> bool:
+        parking = self.get_parking(tenant_uuid, parking_id)
+        total_slots = int(parking.slots_end) - int(parking.slots_start)
+        count = self.count_parked_calls(tenant_uuid, parking_id)
+        return count >= total_slots
+
     def list_parked_calls(
         self, tenant_uuid: str, parking_id: int
     ) -> list[AsteriskParkedCall]:
@@ -165,26 +188,24 @@ class ParkingService:
         ]
         return parked_calls
 
-    def count_parked_calls(self, tenant_uuid: str, parking_id: int) -> int:
-        parking = self.get_parking(tenant_uuid, parking_id)
-
+    def list_parkings(self, tenant_uuid: str) -> list[ConfdParkingLot]:
         try:
-            results: list[dict] = self._amid.action(
-                'ParkedCalls', {'ParkingLot': f'parkinglot-{parking.id}'}
-            )
+            results: list[dict] = self._amid.action('Parkinglots')
         except RequestException as e:
             raise WazoAmidError(self._amid, e)
 
-        complete = results.pop()
-        if complete.get('Event') != 'ParkedCallsComplete':
-            raise WazoAmidError(self._amid, 'missing ParkedCallsComplete message')
-        return int(complete['Total'])
-
-    def is_parking_full(self, tenant_uuid: str, parking_id: int) -> bool:
-        parking = self.get_parking(tenant_uuid, parking_id)
-        total_slots = int(parking.slots_end) - int(parking.slots_start)
-        count = self.count_parked_calls(tenant_uuid, parking_id)
-        return count >= total_slots
+        parkinglots: list[ConfdParkingLot] = []
+        for result in results:
+            if result.get('Event') != 'Parkinglot':
+                continue
+            try:
+                id_ = split_parking_id_from_name(result['Name'])
+            except ValueError:
+                continue
+            parkinglot = self._parkings[id_]
+            if parkinglot.tenant_uuid == tenant_uuid:
+                parkinglots.append(parkinglot)
+        return parkinglots
 
     def park_call(
         self,
