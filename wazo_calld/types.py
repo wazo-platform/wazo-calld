634d6b5e94881fcade57eff7ad6c7e105b518874
Sebastien Duthil <sduthil@wazo.community>
2024-08-13 18:12:52 -0400

    config: add typing

--- wazo_calld/types.py
+++ wazo_calld/types.py
@@ -18,6 +18,113 @@ from .collectd import CollectdPublisher
 TokenRenewalCallback = Callable[[Collection[str]], None]
 
 
+class AmidConfigDict(TypedDict):
+    host: str
+    port: int
+    prefix: str | None
+    https: bool
+
+
+class AriConnectionConfigDict(TypedDict):
+    base_url: str
+    username: str
+    password: str
+
+
+class AriConfigDict(TypedDict):
+    connection: AriConnectionConfigDict
+    reconnection_delay: int
+    startup_connection_delay: int
+
+
+class AuthConfigDict(TypedDict):
+    host: str
+    port: int
+    prefix: str | None
+    https: bool
+    key_file: str
+
+
+class BusConfigDict(TypedDict):
+    username: str
+    password: str
+    host: str
+    port: int
+    exchange_name: str
+    exchange_type: str
+
+
+class CollectdConfigDict(TypedDict):
+    exchange_name: str
+    exchange_type: str
+    exchange_durable: bool
+
+
+class ConfdConfigDict(TypedDict):
+    host: str
+    port: int
+    prefix: str | None
+    https: bool
+
+
+class ConsulConfigDict(TypedDict):
+    scheme: str
+    port: int
+
+
+class PhonedConfigDict(TypedDict):
+    host: str
+    port: int
+    prefix: str | None
+    https: bool
+
+
+class RestApiCorsConfigDict(TypedDict):
+    enabled: bool
+    allow_headers: list[str]
+
+
+class RestApiConfigDict(TypedDict):
+    listen: str
+    port: int
+    certificate: str | None
+    private_key: str | None
+    cors: RestApiCorsConfigDict
+    max_threads: int
+
+
+class ServiceDiscoveryConfigDict(TypedDict):
+    enabled: bool
+    advertise_address: str
+    advertise_address_interface: str
+    advertise_port: int
+    ttl_interval: int
+    refresh_interval: int
+    retry_interval: int
+
+
+class CalldConfigDict(TypedDict):
+    config_file: str
+    extra_config_files: str
+    debug: bool
+    log_level: str
+    log_filename: str
+    user: str
+    amid: AmidConfigDict
+    ari: AriConfigDict
+    auth: AuthConfigDict
+    bus: BusConfigDict
+    collectd: CollectdConfigDict
+    confd: ConfdConfigDict
+    consul: ConsulConfigDict
+    enabled_plugins: dict[str, bool]
+    max_meeting_participants: int
+    phoned: PhonedConfigDict
+    remote_credentials: dict[str, Any]
+    rest_api: RestApiConfigDict
+    service_discovery: ServiceDiscoveryConfigDict
+
+
 class PluginDependencies(TypedDict):
     api: Api
     ari: CoreARI
