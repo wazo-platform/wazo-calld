7b859d463ab3b597adaa07cf2f43a34fb2d6ec78
Sebastien Duthil <sduthil@wazo.community>
2024-08-28 16:36:11 -0400

    typing: homogenize import blank lines

--- integration_tests/assets/ari_data/mock_ari.py
+++ integration_tests/assets/ari_data/mock_ari.py
@@ -1,6 +1,7 @@
 #!/usr/bin/env python3
 # Copyright 2015-2024 The Wazo Authors  (see the AUTHORS file)
 # SPDX-License-Identifier: GPL-3.0-or-later
+
 from __future__ import annotations
 
 import json
