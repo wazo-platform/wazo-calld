51d369b17cb6c4943672f926f4ba629deb42f7e4
Charles Langlois <charles.langlois@wazo.io>
2024-10-09 10:09:40 -0400

    integration_tests: test_transfers: lazy type annotations support

--- integration_tests/suite/test_transfers.py
+++ integration_tests/suite/test_transfers.py
@@ -1,5 +1,6 @@
 # Copyright 2016-2024 The Wazo Authors  (see the AUTHORS file)
 # SPDX-License-Identifier: GPL-3.0-or-later
+from __future__ import annotations
 
 import json
 from typing import Any
