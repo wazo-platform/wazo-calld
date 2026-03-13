2cd2733ccd31199bb91233622163abe4b36e42cf
Charles Langlois <charles.langlois@wazo.io>
2026-03-13 09:41:12 -0400

    voicemail_transcription: WIP

--- wazo_calld/plugins/voicemails/bus_consume.py
+++ wazo_calld/plugins/voicemails/bus_consume.py
@@ -1,4 +1,4 @@
-# Copyright 2016-2025 The Wazo Authors  (see the AUTHORS file)
+# Copyright 2016-2026 The Wazo Authors  (see the AUTHORS file)
 # SPDX-License-Identifier: GPL-3.0-or-later
 
 import logging
@@ -7,6 +7,84 @@ from .schemas import UnifiedVoicemailMessageSchema
 
 logger = logging.getLogger(__name__)
 voicemail_message_schema = UnifiedVoicemailMessageSchema()
+
+
+class TranscriptionBusEventHandler:
+    def __init__(self, confd_client, notifier):
+        self._confd_client = confd_client
+        self._notifier = notifier
+
+    def subscribe(self, bus_consumer):
+        bus_consumer.subscribe(
+            'call_logd_voicemail_transcription_created',
+            self._transcription_created,
+        )
+        bus_consumer.subscribe(
+            'call_logd_voicemail_transcription_deleted',
+            self._transcription_deleted,
+        )
+
+    def _transcription_created(self, event):
+        self._forward_transcription_event(event, 'created')
+
+    def _transcription_deleted(self, event):
+        self._forward_transcription_event(event, 'deleted')
+
+    def _forward_transcription_event(self, event, action):
+        voicemail_id = event.get('voicemail_id')
+        if not voicemail_id:
+            logger.warning(
+                'Received transcription %s event without voicemail_id', action
+            )
+            return
+
+        try:
+            voicemail = self._get_voicemail(voicemail_id)
+        except Exception:
+            logger.warning(
+                'Could not look up voicemail %s for transcription %s event',
+                voicemail_id,
+                action,
+                exc_info=True,
+            )
+            return
+
+        transcription = {
+            'voicemail_id': voicemail_id,
+            'message_id': event.get('voicemail_message_id', ''),
+            'transcription_text': event.get('transcription_text', ''),
+            'provider_id': event.get('provider_id', ''),
+            'language': event.get('language', ''),
+            'duration': event.get('duration', 0.0),
+            'created_at': event.get('created_at', ''),
+        }
+        tenant_uuid = voicemail['tenant_uuid']
+
+        match voicemail['accesstype']:
+            case 'personal':
+                for user in voicemail.get('users', []):
+                    user_uuid = user['uuid']
+                    if action == 'created':
+                        self._notifier.create_user_voicemail_transcription(
+                            user_uuid, tenant_uuid, transcription
+                        )
+                    else:
+                        self._notifier.delete_user_voicemail_transcription(
+                            user_uuid, tenant_uuid, transcription
+                        )
+            case 'global':
+                if action == 'created':
+                    self._notifier.create_global_voicemail_transcription(
+                        tenant_uuid, transcription
+                    )
+                else:
+                    self._notifier.delete_global_voicemail_transcription(
+                        tenant_uuid, transcription
+                    )
+
+    def _get_voicemail(self, voicemail_id):
+        response = self._confd_client.voicemails.get(voicemail_id)
+        return response
 
 
 class VoicemailsBusEventHandler:
