708dd5b9832c6b2199210879d7cbbfb30283b50c
Charles Langlois <charles.langlois@wazo.io>
2026-03-26 14:01:50 -0400

    remove log

--- wazo_calld/plugins/voicemails/plugin.py
+++ wazo_calld/plugins/voicemails/plugin.py
@@ -140,7 +140,6 @@ class Plugin:
             '/users/me/voicemails/messages/<message_id>/recording',
             resource_class_args=[voicemails_service],
         )
-        logger.critical("Loaded transcription-enabled voicemails API")
 
     def _provide_status(self, status: StatusDict) -> None:
         status['plugins']['voicemails']['status'] = Status.ok
