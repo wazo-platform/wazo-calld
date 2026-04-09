1f45f25034a02adc2494fc2a3a4df9bd8ec12cfb
Remy Bienvenu <remy.bienvenu@wazo.io>
2026-04-09 08:47:13 -0700

    recording: Revert auto recording check to called channel
    Why: As is, this change doesn't really make a difference as the auto recording for queues and groups is a grey area but since the current bug isn't linked to that, the behaviour is reverted to avoid side effects
    
    WP-2020

--- wazo_calld/plugins/calls/services.py
+++ wazo_calld/plugins/calls/services.py
@@ -735,7 +735,7 @@ class CallsService:
         if channel_variables['WAZO_CALL_RECORD_ACTIVE'] == '1':
             return
 
-        if self._is_automated_recording(channel.id):
+        if self._is_automated_recording(channel_id):
             logger.debug('bypassing configured toggle permissions for auto-recording')
         elif not self._toggle_record_allowed(channel):
             raise RecordingUnauthorized(call_id)
