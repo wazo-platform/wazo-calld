ed99263859bfca7532cd0a4c51f24b218688466b
Remy Bienvenu <remy.bienvenu@wazo.io>
2026-04-21 13:43:12 -0700

    recording: Update unit tests
    WP-2020

--- wazo_calld/plugin_helpers/tests/test_ari_helpers.py
+++ wazo_calld/plugin_helpers/tests/test_ari_helpers.py
@@ -1,4 +1,4 @@
-# Copyright 2019-2024 The Wazo Authors  (see the AUTHORS file)
+# Copyright 2019-2026 The Wazo Authors  (see the AUTHORS file)
 # SPDX-License-Identifier: GPL-3.0-or-later
 
 from unittest import TestCase
@@ -6,7 +6,7 @@ from unittest.mock import Mock
 from unittest.mock import sentinel as s
 
 from ari.exceptions import ARINotFound
-from hamcrest import assert_that, equal_to
+from hamcrest import assert_that, equal_to, is_
 
 from ..ari_ import Channel
 
@@ -52,3 +52,49 @@ class TestChannelHelper(TestCase):
         result = channel.dialed_extension()
 
         assert_that(result, equal_to(s.exten))
+
+    def test_is_group_auto_recorded_returns_true_when_shared_var_is_1(self):
+        self.ari.channels.getChannelVar.return_value = {'value': '1'}
+
+        channel = Channel(s.channel_id, self.ari)
+        result = channel.is_group_auto_recorded()
+
+        assert_that(result, is_(True))
+        self.ari.channels.getChannelVar.assert_called_once_with(
+            channelId=s.channel_id,
+            variable='SHARED(WAZO_RECORD_GROUP_CALLEE)',
+        )
+
+    def test_is_group_auto_recorded_returns_false_when_not_set(self):
+        self.ari.channels.getChannelVar.side_effect = ARINotFound(
+            original_error='no var',
+            ari_client=self.ari,
+        )
+
+        channel = Channel(s.channel_id, self.ari)
+        result = channel.is_group_auto_recorded()
+
+        assert_that(result, is_(False))
+
+    def test_is_queue_auto_recorded_returns_true_when_shared_var_is_1(self):
+        self.ari.channels.getChannelVar.return_value = {'value': '1'}
+
+        channel = Channel(s.channel_id, self.ari)
+        result = channel.is_queue_auto_recorded()
+
+        assert_that(result, is_(True))
+        self.ari.channels.getChannelVar.assert_called_once_with(
+            channelId=s.channel_id,
+            variable='SHARED(WAZO_RECORD_QUEUE_CALLEE)',
+        )
+
+    def test_is_queue_auto_recorded_returns_false_when_not_set(self):
+        self.ari.channels.getChannelVar.side_effect = ARINotFound(
+            original_error='no var',
+            ari_client=self.ari,
+        )
+
+        channel = Channel(s.channel_id, self.ari)
+        result = channel.is_queue_auto_recorded()
+
+        assert_that(result, is_(False))
