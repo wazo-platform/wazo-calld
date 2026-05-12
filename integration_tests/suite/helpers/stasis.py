14cad14e411373d83a97237d901bb7eed2be31f1
Alexandre Fournier <afournier@wazo.io>
2026-05-12 15:55:26 -0400

    switchboards: update dates

--- integration_tests/suite/helpers/stasis.py
+++ integration_tests/suite/helpers/stasis.py
@@ -1,4 +1,4 @@
-# Copyright 2015-2024 The Wazo Authors  (see the AUTHORS file)
+# Copyright 2015-2026 The Wazo Authors  (see the AUTHORS file)
 # SPDX-License-Identifier: GPL-3.0-or-later
 
 from wazo_bus.publisher import BusPublisher
--- wazo_calld/plugins/switchboards/stasis.py
+++ wazo_calld/plugins/switchboards/stasis.py
@@ -1,4 +1,4 @@
-# Copyright 2017-2024 The Wazo Authors  (see the AUTHORS file)
+# Copyright 2017-2026 The Wazo Authors  (see the AUTHORS file)
 # SPDX-License-Identifier: GPL-3.0-or-later
 
 import logging
